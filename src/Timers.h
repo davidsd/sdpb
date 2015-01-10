d9de8d1115e8db3ec8d1ec079ce85bd3862da7a8
David Simmons-Duffin <dsd@minerva.sns.ias.edu>
2015-01-10 04:02:45 -0500

    Commented Timers.h

--- src/Timers.h
+++ src/Timers.h
@@ -20,8 +20,10 @@ using std::string;
 using std::ostream;
 using boost::timer::cpu_timer;
 
+// A map between strings and cpu timers
 class Timers : public map<string, cpu_timer> {
  public:
+  // For printing out timing information
   friend ostream& operator<<(ostream& os, const Timers& t) {
     for (map<string, cpu_timer>::const_iterator it = t.begin();
          it != t.end();
@@ -32,6 +34,18 @@ class Timers : public map<string, cpu_timer> {
   }
 };
 
+// A global Timers map for the whole program (defined in main.cpp).  A
+// new timer is created by default whenver `timers' is accessed.  To
+// time something, simply replace
+//
+//   myComputation();
+//
+// with
+//
+//   timers["my computation"].start();
+//   myComputation();
+//   timers["my computation"].stop();
+//
 extern Timers timers;
 
 #endif  // SDPB_TIMERS_H_
