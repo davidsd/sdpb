b86930d9177cf8f6d24923bbc30772e39df77555
Walter Landry <wlandry@caltech.edu>
2018-04-23 11:36:59 -0700

    Rename A to Z in bilinear_block_pairing() to match how it is called

--- src/solve/SDP_Solver/run/compute_search_direction/compute_schur_RHS/bilinear_block_pairing.cxx
+++ src/solve/SDP_Solver/run/compute_search_direction/compute_schur_RHS/bilinear_block_pairing.cxx
@@ -1,17 +1,17 @@
 #include "../../../../SDP_Solver.hxx"
 
-// v^T A^(blockRow, blockCol) v, where A^(r,s) is the (r,s)-th dim x
-// dim block inside A.
+// v^T Z^(blockRow, blockCol) v, where Z^(r,s) is the (r,s)-th dim x
+// dim block inside Z.
 //
 // Input:
 // - v        : pointer to the beginning of a vector of length dim
 // - dim      : length of the vector v
-// - A        : (k*dim) x (k*dim) matrix, where k > blockRow, blockCol
-// - blockRow : integer labeling block of A
-// - blockCol : integer labeling block of A
-// Output: v^T A^(blockRow, blockCol) v (returned)
+// - Z        : (k*dim) x (k*dim) matrix, where k > blockRow, blockCol
+// - blockRow : integer labeling block of Z
+// - blockCol : integer labeling block of Z
+// Output: v^T Z^(blockRow, blockCol) v (returned)
 //
-Real bilinear_block_pairing(const Real *v, const int dim, const Matrix &A,
+Real bilinear_block_pairing(const Real *v, const int dim, const Matrix &Z,
                             const int blockRow, const int blockCol)
 {
   Real result = 0;
@@ -21,7 +21,7 @@ Real bilinear_block_pairing(const Real *v, const int dim, const Matrix &A,
       Real tmp = 0;
 
       for(int c = 0; c < dim; c++)
-        tmp += *(v + c) * A.elt(blockRow * dim + r, blockCol * dim + c);
+        tmp += *(v + c) * Z.elt(blockRow * dim + r, blockCol * dim + c);
       result += *(v + r) * tmp;
     }
   return result;
