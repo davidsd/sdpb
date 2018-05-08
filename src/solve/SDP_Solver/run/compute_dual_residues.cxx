2e26e5234789bbaabc145c4471d8105d3a0cd1d2
Walter Landry <wlandry@caltech.edu>
2018-05-08 00:03:00 -0700

    Simplify compute_dual_residues to only use BLAS-like operations

--- src/solve/SDP_Solver/run/compute_dual_residues.cxx
+++ src/solve/SDP_Solver/run/compute_dual_residues.cxx
@@ -57,80 +57,54 @@ void compute_dual_residues(const SDP &sdp,
                            const Block_Diagonal_Matrix &bilinear_pairings_Y,
                            Block_Matrix &dual_residues)
 {
-  // Maps from linear subblock offsets in dual_residues to subblock
-  // indices (r,s) for bilinear_pairings_Y.
-  //
-  // This is only valid up to 5x5 blocks, but it should fail noisily if it is
-  // out of bounds.
-  const std::vector<size_t> r_map(
-    {0, 0, 1, 0, 1, 2, 0, 1, 2, 3, 0, 1, 2, 3, 4}),
-    s_map({0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4});
-
-  // Set up all of the queues to fetch the diagonals and off-diagonals
-  // of bilinear_pairings_Y for Tr(A_p Y).  For small blocks, this
-  // is probably all local.
+  // dualResidues[p] = -Tr(A_p Y)
   for(size_t jj = 0; jj < sdp.dimensions.size(); ++jj)
     {
-      const size_t bilinear_bases_block_index(2 * jj);
-      const size_t bilinear_bases_row_size(
-        sdp.bilinear_bases_elemental_local[bilinear_bases_block_index].Height()
-        * sdp.dimensions[jj]);
+      Zero(dual_residues.blocks[jj]);
 
-      const size_t row_offset(dual_residues.blocks[jj].GlobalRow(0));
-      const size_t local_height(dual_residues.blocks[jj].LocalHeight());
-      for(size_t row = 0; row < local_height; ++row)
+      const size_t block_size(sdp.degrees[jj]+1);
+
+      // Not sure whether it is better to first loop over blocks in
+      // the result or over sub-blocks in bilinear_pairings_Y
+      for(size_t block_index = 2 * jj; block_index < 2 * jj + 2; ++block_index)
         {
-          const size_t global_row(row + row_offset);
-          const size_t subblock_index(global_row / bilinear_bases_row_size),
-            k(global_row % bilinear_bases_row_size);
-          const size_t r(r_map.at(subblock_index)),
-            s(s_map.at(subblock_index));
-          const size_t r_k(r * bilinear_bases_row_size + k),
-            s_k(s * bilinear_bases_row_size + k);
+          for(size_t column_block = 0; column_block < sdp.dimensions[jj];
+              ++column_block)
+            for(size_t row_block = 0; row_block <= column_block; ++row_block)
+              {
+                size_t column_offset(column_block * block_size),
+                  row_offset(row_block * block_size);
 
-          bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index]
-            .QueuePull(r_k, s_k);
-          bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index]
-            .QueuePull(s_k, r_k);
-          bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index + 1]
-            .QueuePull(r_k, s_k);
-          bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index + 1]
-            .QueuePull(s_k, r_k);
+                El::DistMatrix<El::BigFloat> lower_diagonal(
+                  El::GetDiagonal(El::LockedView(
+                    bilinear_pairings_Y.blocks_elemental[block_index],
+                    row_offset, column_offset, block_size, block_size))),
+                  upper_diagonal(El::GetDiagonal(El::LockedView(
+                    bilinear_pairings_Y.blocks_elemental[block_index],
+                    column_offset, row_offset, block_size, block_size)));
+
+                size_t residue_row_offset(
+                  ((column_block * (column_block + 1)) / 2 + row_block)
+                  * block_size);
+
+                El::DistMatrix<El::BigFloat> residue_sub_block(
+                  El::View(dual_residues.blocks[jj], residue_row_offset, 0));
+                
+                El::Axpy(El::BigFloat(-0.5), lower_diagonal,
+                         residue_sub_block);
+                El::Axpy(El::BigFloat(-0.5), upper_diagonal,
+                         residue_sub_block);
+              }
         }
     }
 
-  // dualResidues = primalObjective - (FreeVarMatrix y)
+  // dualResidues += primalObjective - (FreeVarMatrix y)
   for(size_t b = 0; b < dual_residues.blocks.size(); ++b)
     {
       Gemm(El::Orientation::NORMAL, El::Orientation::NORMAL, El::BigFloat(-1),
-           sdp.free_var_matrix_elemental.blocks[b], y, El::BigFloat(0),
+           sdp.free_var_matrix_elemental.blocks[b], y, El::BigFloat(1),
            dual_residues.blocks[b]);
       Axpy(El::BigFloat(1), sdp.primal_objective_c_elemental.blocks[b],
            dual_residues.blocks[b]);
-    }
-
-  // dualResidues -= Tr(A_p Y)
-  //
-  // Doing this later helps overlap communication and computation
-  // (maybe?)
-  std::vector<El::BigFloat> even_queue, odd_queue;
-  for(size_t jj = 0; jj < sdp.dimensions.size(); ++jj)
-    {
-      const size_t bilinear_bases_block_index(2 * jj);
-      bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index]
-        .ProcessPullQueue(even_queue);
-      bilinear_pairings_Y.blocks_elemental[bilinear_bases_block_index+1]
-        .ProcessPullQueue(odd_queue);
-
-      const size_t local_height(dual_residues.blocks[jj].LocalHeight());
-      for(size_t row = 0; row < local_height; ++row)
-        {
-          const size_t queue_index(2 * row);
-          dual_residues.blocks[jj].UpdateLocal(
-            row, 0,
-            (even_queue.at(queue_index) + even_queue.at(queue_index + 1)
-             + odd_queue.at(queue_index) + odd_queue.at(queue_index + 1))
-              / -2);
-        }
     }
 }
