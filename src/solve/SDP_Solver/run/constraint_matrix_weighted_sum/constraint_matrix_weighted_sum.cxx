e40b5d599a94cd7a13acea0ab3794dd5afec4e36
Walter Landry <wlandry@caltech.edu>
2018-05-08 21:34:15 -0700

    Use a proper result sub_block in constraint_matrix_weighted_sum

--- src/solve/SDP_Solver/run/constraint_matrix_weighted_sum/constraint_matrix_weighted_sum.cxx
+++ src/solve/SDP_Solver/run/constraint_matrix_weighted_sum/constraint_matrix_weighted_sum.cxx
@@ -77,23 +77,29 @@ void constraint_matrix_weighted_sum(const SDP &sdp, const Block_Matrix &a,
               ++column_block)
             for(size_t row_block = 0; row_block <= column_block; ++row_block)
               {
-                size_t row_offset(
+                const size_t result_block_size(
+                  sdp.bilinear_bases_elemental_dist[block_index].Height());
+                const size_t column_offset(column_block * result_block_size),
+                  row_offset(row_block * result_block_size);
+                size_t vector_offset(
                   ((column_block * (column_block + 1)) / 2 + row_block)
                   * block_size);
-                El::DistMatrix<El::BigFloat> sub_vector(
-                  El::LockedView(a.blocks[jj], row_offset, 0, block_size, 1));
+                El::DistMatrix<El::BigFloat> sub_vector(El::LockedView(
+                  a.blocks[jj], vector_offset, 0, block_size, 1));
                 El::DistMatrix<El::BigFloat> scaled_bases(
                   sdp.bilinear_bases_elemental_dist[block_index]);
-      
+
                 El::DiagonalScale(El::LeftOrRight::RIGHT,
                                   El::Orientation::NORMAL, sub_vector,
                                   scaled_bases);
 
+                El::DistMatrix<El::BigFloat> result_sub_block(El::View(
+                  result.blocks_elemental[block_index], row_offset,
+                  column_offset, result_block_size, result_block_size));
                 El::Gemm(El::Orientation::NORMAL, El::Orientation::TRANSPOSE,
                          El::BigFloat(1),
                          sdp.bilinear_bases_elemental_dist[block_index],
-                         scaled_bases, El::BigFloat(0),
-                         result.blocks_elemental[block_index]);
+                         scaled_bases, El::BigFloat(0), result_sub_block);
               }
         }
     }
