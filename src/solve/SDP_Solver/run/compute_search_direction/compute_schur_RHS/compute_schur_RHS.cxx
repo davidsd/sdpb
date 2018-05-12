6ba7400ad6daee361d45ea2556c38b8cc7364919
Walter Landry <wlandry@caltech.edu>
2018-05-11 18:31:58 -0700

    Compute r_x_elemental and r_y_elemental

--- src/solve/SDP_Solver/run/compute_search_direction/compute_schur_RHS/compute_schur_RHS.cxx
+++ src/solve/SDP_Solver/run/compute_search_direction/compute_schur_RHS/compute_schur_RHS.cxx
@@ -36,7 +36,7 @@ void compute_schur_RHS(const SDP &sdp, const Vector &dual_residues,
   for(unsigned int p = 0; p < r_x.size(); p++)
     r_x[p] = -dual_residues[p];
 
-  // r_x[p] = -dual_residues[p] - Tr(A_p Z), where A_p are as described
+  // r_x[p] -= Tr(A_p Z), where A_p are as described
   // in SDP.h.  The trace can be computed in terms of bilinearBases
   // using bilinearBlockPairing.
   for(size_t j = 0; j < sdp.dimensions.size(); j++)
@@ -53,7 +53,60 @@ void compute_schur_RHS(const SDP &sdp, const Vector &dual_residues,
         }
     }
 
-  // r_y[n] = dualObjective[n] - (FreeVarMatrix^T x)_n
+  for(size_t jj = 0; jj < sdp.dimensions.size(); ++jj)
+    {
+      // r_x = -dual_residues
+      r_x_elemental.blocks[jj] = dual_residues_elemental.blocks[jj];
+      r_x_elemental.blocks[jj] *= -1;
+      const size_t r_x_block_size(sdp.degrees[jj] + 1);
+
+      // r_x[p] -= Tr(A_p Z)
+      // Not sure whether it is better to first loop over blocks in
+      // the result or over sub-blocks in Z
+      for(size_t block_index = 2 * jj; block_index < 2 * jj + 2; ++block_index)
+        {
+          const size_t Z_block_size(
+            sdp.bilinear_bases_elemental_dist[block_index].Height());
+          El::DistMatrix<El::BigFloat> ones;
+          El::Ones(ones, Z_block_size, 1);
+
+          for(size_t column_block = 0; column_block < sdp.dimensions[jj];
+              ++column_block)
+            for(size_t row_block = 0; row_block <= column_block; ++row_block)
+              {
+                size_t column_offset(column_block * Z_block_size),
+                  row_offset(row_block * Z_block_size);
+
+                El::DistMatrix<El::BigFloat> Z_sub_block(
+                  El::LockedView(Z.blocks_elemental[block_index], row_offset,
+                                 column_offset, Z_block_size, Z_block_size)),
+                  Z_times_q(Z_block_size, r_x_block_size),
+                  q_Z_q(Z_block_size, r_x_block_size);
+                El::Zero(Z_times_q);
+                El::Zero(q_Z_q);
+
+                El::Gemm(El::Orientation::NORMAL, El::Orientation::NORMAL,
+                         El::BigFloat(1), Z_sub_block,
+                         sdp.bilinear_bases_elemental_dist[block_index],
+                         El::BigFloat(0), Z_times_q);
+
+                El::Hadamard(Z_times_q,
+                             sdp.bilinear_bases_elemental_dist[block_index],
+                             q_Z_q);
+
+                const size_t r_x_row_offset(
+                  ((column_block * (column_block + 1)) / 2 + row_block)
+                  * Z_block_size);
+                El::DistMatrix<El::BigFloat> r_x_sub_block(El::View(
+                  r_x_elemental.blocks[jj], r_x_row_offset, 0, r_x_block_size, 1));
+
+                El::Gemv(El::Orientation::TRANSPOSE, El::BigFloat(-1), q_Z_q,
+                         ones, El::BigFloat(1), r_x_sub_block);
+              }
+        }
+    }
+
+  // r_y = dualObjective - (FreeVarMatrix^T x)
   for(int n = 0; n < sdp.dual_objective_b_elemental.Height(); n++)
     {
       r_y[n] = sdp.dual_objective_b[n];
