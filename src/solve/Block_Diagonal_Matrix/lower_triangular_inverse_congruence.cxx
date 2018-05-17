1ab83e6d8a6b34d3c2d70133d05bcc50267d644f
Walter Landry <wlandry@caltech.edu>
2018-05-16 18:08:00 -0700

    Add Elemental logic to lower_triangular_inverse_congruence

--- src/solve/Block_Diagonal_Matrix/lower_triangular_inverse_congruence.cxx
+++ src/solve/Block_Diagonal_Matrix/lower_triangular_inverse_congruence.cxx
@@ -7,5 +7,12 @@ void lower_triangular_inverse_congruence(Block_Diagonal_Matrix &A,
   for(size_t b = 0; b < A.blocks.size(); b++)
     {
       lower_triangular_inverse_congruence(A.blocks[b], L.blocks[b]);
+
+      El::Trsm(El::LeftOrRight::RIGHT, El::UpperOrLowerNS::LOWER,
+               El::Orientation::TRANSPOSE, El::UnitOrNonUnit::NON_UNIT,
+               El::BigFloat(1), L.blocks_elemental[b], A.blocks_elemental[b]);
+      El::Trsm(El::LeftOrRight::LEFT, El::UpperOrLowerNS::LOWER,
+               El::Orientation::NORMAL, El::UnitOrNonUnit::NON_UNIT,
+               El::BigFloat(1), L.blocks_elemental[b], A.blocks_elemental[b]);
     }
 }
