75b4d013e410fa67933751bdef733bfc339e046c
Walter Landry <wlandry@caltech.edu>
2018-06-12 12:51:04 -0700

    Get rid of the local copy shenanigans in min_eigenvalue
    The bug that required that workaround was fixed in 73488ae137f09f50d68dfccde2fff92d486996b8

--- src/solve/Block_Diagonal_Matrix/min_eigenvalue.cxx
+++ src/solve/Block_Diagonal_Matrix/min_eigenvalue.cxx
@@ -1,46 +1,20 @@
 #include "../Block_Diagonal_Matrix.hxx"
 
-// Minimum eigenvalue of A, via the QR method
-// Inputs:
-// A           : symmetric Block_Diagonal_Matrix
-//
+// Minimum eigenvalue of A.  A is assumed to be symmetric.
+
 El::BigFloat min_eigenvalue(Block_Diagonal_Matrix &A)
 {
   El::BigFloat lambda_min(El::limits::Max<El::BigFloat>());
 
-  // FIXME: El::HermitianEig() seems broken in parallel.  So this
-  // makes a local copy of the blocks before computing the
-  // eigenvalues.  So it is reeeeeeeealy inefficient.
-
-  // El::DistMatrix<El::BigFloat> eigenvalues;
-  El::Matrix<El::BigFloat> local_eigenvalues;
-  for(auto &block : A.blocks_elemental)
+  for(auto block=A.blocks_elemental.begin(); block!=A.blocks_elemental.end(); ++block)
     {
-      El::Matrix<El::BigFloat> local_copy(block.Height(), block.Width());
-      block.ReservePulls(local_copy.Height() * local_copy.Width());
+      El::DistMatrix<El::BigFloat,El::VR,El::STAR> eigenvalues( block->Grid() );
 
-      for(size_t row = 0; row < local_copy.Height(); ++row)
-        for(size_t column = 0; column < local_copy.Width(); ++column)
-          {
-            block.QueuePull(row, column);
-          }
-      std::vector<El::BigFloat> pull_queue;
-      block.ProcessPullQueue(pull_queue);
+      El::DistMatrix<El::BigFloat> block_copy(block->Height(),block->Width());
+      block_copy=*block;
 
-      size_t queue_index(0);
-      for(size_t row = 0; row < local_copy.Height(); ++row)
-        for(size_t column = 0; column < local_copy.Width(); ++column)
-          {
-            local_copy.Set(row, column, pull_queue[queue_index]);
-            ++queue_index;
-          }
-      El::HermitianEig(El::UpperOrLowerNS::LOWER, local_copy,
-                       local_eigenvalues);
-      lambda_min = El::Min(lambda_min, El::Min(local_eigenvalues));
-
-      /// FIXME: Maybe use regular double precision here?
-      // El::HermitianEig(El::UpperOrLowerNS::LOWER, block, eigenvalues);
-      // lambda_min = El::Min(lambda_min, El::Min(eigenvalues));
+      El::HermitianEig(El::UpperOrLowerNS::LOWER, block_copy, eigenvalues);
+      lambda_min = El::Min(lambda_min, El::Min(eigenvalues));
     }
   return lambda_min;
 }
