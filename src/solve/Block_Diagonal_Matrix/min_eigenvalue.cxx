f12c370f12b5848a6ba99969a4b21d8258adc50f
Walter Landry <wlandry@caltech.edu>
2018-06-12 12:56:25 -0700

    Remove the copy in min_eigenvalue

--- src/solve/Block_Diagonal_Matrix/min_eigenvalue.cxx
+++ src/solve/Block_Diagonal_Matrix/min_eigenvalue.cxx
@@ -2,18 +2,17 @@
 
 // Minimum eigenvalue of A.  A is assumed to be symmetric.
 
+// Annoyingly, El::HermitianEig modifies 'block'.  It is OK, because
+// it is only called from step_length(), which passes in a temporary.
+// Still ugly.
 El::BigFloat min_eigenvalue(Block_Diagonal_Matrix &A)
 {
   El::BigFloat lambda_min(El::limits::Max<El::BigFloat>());
 
-  for(auto block=A.blocks_elemental.begin(); block!=A.blocks_elemental.end(); ++block)
+  for(auto &block: A.blocks_elemental)
     {
-      El::DistMatrix<El::BigFloat,El::VR,El::STAR> eigenvalues( block->Grid() );
-
-      El::DistMatrix<El::BigFloat> block_copy(block->Height(),block->Width());
-      block_copy=*block;
-
-      El::HermitianEig(El::UpperOrLowerNS::LOWER, block_copy, eigenvalues);
+      El::DistMatrix<El::BigFloat,El::VR,El::STAR> eigenvalues( block.Grid() );
+      El::HermitianEig(El::UpperOrLowerNS::LOWER, block, eigenvalues);
       lambda_min = El::Min(lambda_min, El::Min(eigenvalues));
     }
   return lambda_min;
