72335bcc9eb7cb9af15fd210be03e9aa7efda19d
Walter Landry <wlandry@caltech.edu>
2018-07-06 10:11:00 -0700

    Check for c++14 in elemental.py

--- elemental.py
+++ elemental.py
@@ -5,6 +5,8 @@ def configure(conf):
     def get_param(varname,default):
         return getattr(Options.options,varname,'')or default
 
+    conf.load('compiler_cxx cxx14')
+    
     # Find Elemental
     if conf.options.elemental_dir:
         if not conf.options.elemental_incdir:
@@ -13,19 +15,19 @@ def configure(conf):
             conf.options.elemental_libdir=conf.options.elemental_dir + "/lib"
 
     if conf.options.elemental_incdir:
-        elemental_incdir=[conf.options.elemental_incdir]
+        elemental_incdir=conf.options.elemental_incdir.split()
     else:
         elemental_incdir=[]
     if conf.options.elemental_libdir:
-        elemental_libdir=[conf.options.elemental_libdir]
+        elemental_libdir=conf.options.elemental_libdir.split()
     else:
         elemental_libdir=[]
 
     if conf.options.elemental_libs:
         elemental_libs=conf.options.elemental_libs.split()
     else:
-        elemental_libs=['El', 'pmrrr', 'ElSuiteSparse', 'pthread', 'm', 'qd', 'mpc',
-                        'mpfr', 'gmp', 'parmetis', 'metis' ]
+        elemental_libs=['El', 'pmrrr', 'ElSuiteSparse', 'pthread', 'm', 'mpc',
+                        'mpfr', 'gmp', 'metis' ]
 
     conf.check_cxx(msg="Checking for Elemental",
                    header_name='El.hpp',
@@ -33,7 +35,8 @@ def configure(conf):
                    uselib_store='elemental',
                    libpath=elemental_libdir,
                    rpath=elemental_libdir,
-                   lib=elemental_libs)
+                   lib=elemental_libs,
+                   use='cxx14')
 
 def options(opt):
     elemental=opt.add_option_group('Elemental Options')
