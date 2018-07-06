d1eda5ac3eabbee9e573df8c6ea7065108275af7
Walter Landry <wlandry@caltech.edu>
2018-07-06 11:35:51 -0700

    Make gmpxx detection look at environment variables

--- gmpxx.py
+++ gmpxx.py
@@ -1,10 +1,31 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
+import os
+
 def configure(conf):
     def get_param(varname,default):
         return getattr(Options.options,varname,'')or default
 
+    if not conf.options.gmpxx_incdir:
+        for d in ['GMP_INCLUDE','GMPXX_INCLUDE','GMP_INCLUDE_DIR',
+                  'GMPXX_INCLUDE_DIR','GMP_INC_DIR','GMPXX_INC_DIR']:
+            env_dir=os.getenv(d)
+            if env_dir:
+                conf.options.gmpxx_incdir=env_dir
+                
+    if not conf.options.gmpxx_libdir:
+        for d in ['GMP_LIB','GMPXX_LIB','GMP_LIB_DIR','GMPXX_LIB_DIR']:
+            env_dir=os.getenv(d)
+            if env_dir:
+                conf.options.gmpxx_incdir=env_dir
+
+    if not conf.options.gmpxx_dir:
+        for d in ['GMP_DIR','GMPXX_DIR']:
+            env_dir=os.getenv(d)
+            if env_dir:
+                conf.options.gmpxx_incdir=env_dir
+                
     # Find GMPXX
     if conf.options.gmpxx_dir:
         if not conf.options.gmpxx_incdir:
@@ -13,11 +34,11 @@ def configure(conf):
             conf.options.gmpxx_libdir=conf.options.gmpxx_dir + "/lib"
 
     if conf.options.gmpxx_incdir:
-        gmpxx_incdir=[conf.options.gmpxx_incdir]
+        gmpxx_incdir=conf.options.gmpxx_incdir.split()
     else:
         gmpxx_incdir=[]
     if conf.options.gmpxx_libdir:
-        gmpxx_libdir=[conf.options.gmpxx_libdir]
+        gmpxx_libdir=conf.options.gmpxx_libdir.split()
     else:
         gmpxx_libdir=[]
 
