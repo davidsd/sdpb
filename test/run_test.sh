53e33a1745c476582486a7a1333b27a1d42da947
Walter Landry <wlandry@caltech.edu>
2018-08-24 10:02:45 -0700

    Update test

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,7 +3,7 @@
 # Run this from the top level directory
 
 rm -f test/test.out
-./build/sdpb --precision=1024 --noFinalCheckpoint -s test/test.xml > /dev/null
+./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test/ > /dev/null
 diff test/test.out test/test.out.orig
 if [ $? == 0 ]
 then
