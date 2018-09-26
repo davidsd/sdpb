e7fad6d39da76af79581e645ee4bbb93075a1f81
Walter Landry <wlandry@caltech.edu>
2018-09-25 17:59:19 -0700

    Implement reading checkpoint files

--- src/sdpb/solve/SDP_Solver/load_checkpoint.cxx
+++ src/sdpb/solve/SDP_Solver/load_checkpoint.cxx
@@ -2,14 +2,54 @@
 
 #include <boost/archive/text_iarchive.hpp>
 
-void SDP_Solver::load_checkpoint(const boost::filesystem::path &checkpoint_file)
+template <typename T>
+void read_local_blocks(T &t,
+                       boost::filesystem::ifstream &checkpoint_stream)
 {
-  boost::filesystem::ifstream ifs(checkpoint_file);
-  boost::archive::text_iarchive ar(ifs);
+  for(auto &block : t.blocks)
+    {
+      int64_t local_height, local_width;
+      checkpoint_stream >> local_height >> local_width;
+      if(local_height != block.LocalHeight()
+         || local_width != block.LocalWidth())
+        {
+          std::stringstream ss;
+          ss << "Incompatible checkpoint file.  Expected dimensions ("
+             << block.LocalHeight() << "," << block.LocalWidth()
+             << "), but found (" << local_height << "," << local_width << ")";
+
+          throw std::runtime_error(ss.str());
+        }
+
+      for(int64_t row = 0; row < local_height; ++row)
+        for(int64_t column = 0; column < local_width; ++column)
+          {
+            El::BigFloat input;
+            checkpoint_stream >> input;
+            block.SetLocal(row, column, input);
+          }
+    }
+}
+
+void SDP_Solver::load_checkpoint(
+  const boost::filesystem::path &checkpoint_directory)
+{
+  boost::filesystem::path checkpoint_filename(
+    checkpoint_directory / ("checkpoint." + std::to_string(El::mpi::Rank())));
+
+  if(!exists(checkpoint_filename))
+    {
+      return;
+    }
+
+  boost::filesystem::ifstream checkpoint_stream(checkpoint_filename);
   if(El::mpi::Rank() == 0)
     {
-      std::cout << "Loading checkpoint from : " << checkpoint_file << '\n';
+      std::cout << "Loading checkpoint from : " << checkpoint_directory
+                << '\n';
     }
-  // boost::serialization::serialize_SDP_solver_state(ar, x, X,
-  //                                                  y, Y);
+  read_local_blocks(x, checkpoint_stream);
+  read_local_blocks(X, checkpoint_stream);
+  read_local_blocks(y, checkpoint_stream);
+  read_local_blocks(Y, checkpoint_stream);
 }
