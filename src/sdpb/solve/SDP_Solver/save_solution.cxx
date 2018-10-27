ee2f21b025f07ef6fcfc30c46a3d0f98a428df3d
Walter Landry <wlandry@caltech.edu>
2018-10-27 14:13:00 -0700

    Fix output corruption

--- src/sdpb/solve/SDP_Solver/save_solution.cxx
+++ src/sdpb/solve/SDP_Solver/save_solution.cxx
@@ -5,40 +5,41 @@ void SDP_Solver::save_solution(
   const SDP_Solver_Terminate_Reason terminate_reason,
   const boost::filesystem::path &out_file) const
 {
-  // El::Print requires all processors, but we do not want all processors
-  // writing header information.
-  // FIXME: Write separate files for each rank.
+  // Internally, El::Print() sync's everything to the root core and
+  // outputs it from there.  So do not actually open the file on
+  // anything but the root node.
 
-  boost::filesystem::ofstream ofs(out_file);
-  set_stream_precision(ofs);
+  boost::filesystem::ofstream out_stream;
   if(El::mpi::Rank() == 0)
     {
       std::cout << "Saving solution to      : " << out_file << '\n';
-      ofs << "terminateReason = \"" << terminate_reason << "\";\n"
-          << "primalObjective = " << primal_objective << ";\n"
-          << "dualObjective   = " << dual_objective << ";\n"
-          << "dualityGap      = " << duality_gap << ";\n"
-          << "primalError     = " << primal_error << ";\n"
-          << "dualError       = " << dual_error << ";\n"
-          << "y = {";
+      out_stream.open(out_file);
+      set_stream_precision(out_stream);
+      out_stream << "terminateReason = \"" << terminate_reason << "\";\n"
+                 << "primalObjective = " << primal_objective << ";\n"
+                 << "dualObjective   = " << dual_objective << ";\n"
+                 << "dualityGap      = " << duality_gap << ";\n"
+                 << "primalError     = " << primal_error << ";\n"
+                 << "dualError       = " << dual_error << ";\n"
+                 << "y = {";
     }
   if(!y.blocks.empty())
     {
-      El::Print(y.blocks.at(0), "", ofs);
+      El::Print(y.blocks.at(0), "", out_stream);
     }
 
   if(El::mpi::Rank() == 0)
     {
-      ofs << "};\nx = {";
+      out_stream << "};\nx = {";
     }
 
   for(auto &block : x.blocks)
     {
-      El::Print(block, "", ofs);
+      El::Print(block, "", out_stream);
     }
 
   if(El::mpi::Rank() == 0)
     {
-      ofs << "};\n";
+      out_stream << "};\n";
     }
 }
