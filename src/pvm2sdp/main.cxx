bc5b8c349315aab9abb46a598ac0ad1119a4bb5c
Walter Landry <wlandry@caltech.edu>
2018-11-19 15:21:49 -0800

    Check for multiple MPI tasks in pvm2sdp

--- src/pvm2sdp/main.cxx
+++ src/pvm2sdp/main.cxx
@@ -37,6 +37,19 @@ int main(int argc, char **argv)
 {
   El::Environment env(argc, argv);
 
+  if(El::mpi::Size(El::mpi::COMM_WORLD) != 1)
+    {
+      if(El::mpi::Rank() == 0)
+        {
+          std::cerr << "pvm2sdp can only be run with a single MPI task, but "
+                       "was invoked with "
+                    << El::mpi::Size(El::mpi::COMM_WORLD) << " tasks.\n"
+                    << std::flush;
+        }
+      El::Finalize();
+      exit(-1);
+    }
+
   int result(0);
   try
     {
