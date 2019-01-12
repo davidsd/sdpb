16a32ec89722bda9c33e4a8dbe5ca40551e85085
Walter Landry <wlandry@caltech.edu>
2019-01-11 22:01:11 -0800

    Reorganize a bit

--- src/sdp2blocks/read_input/Input_Parser/on_characters.cxx
+++ src/sdp2blocks/read_input/Input_Parser/on_characters.cxx
@@ -2,7 +2,9 @@
 
 void Input_Parser::on_characters(const xmlChar *characters, int length)
 {
-  if(inside_expression && inside_sdp
-     && !objective_state.on_characters(characters, length)
-     && !normalization_state.on_characters(characters, length))
-    {} }
+  if(inside_expression && inside_sdp)
+    {
+      objective_state.on_characters(characters, length)
+        || normalization_state.on_characters(characters, length);
+    }
+}
