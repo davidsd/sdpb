10f48e29c1c61c3fabf90ddfb460fb88a52d2b66
Walter Landry <wlandry@caltech.edu>
2019-03-22 22:40:01 -0700

    Optimize filling dy_dist
    Instead of sending out every contribution to dy_dist, compute
    local sums first.  Then send out the sums.

--- src/sdpb/solve/SDP_Solver/run/step/compute_search_direction/solve_schur_complement_equation.cxx
+++ src/sdpb/solve/SDP_Solver/run/step/compute_search_direction/solve_schur_complement_equation.cxx
@@ -17,33 +17,49 @@ void solve_schur_complement_equation(
   const Block_Matrix &schur_off_diagonal,
   const El::DistMatrix<El::BigFloat> &Q, Block_Vector &dx, Block_Vector &dy)
 {
-  // dx = SchurComplementCholesky^{-1} dx
+  // Set dx to SchurComplementCholesky^{-1} dx
   lower_triangular_solve(schur_complement_cholesky, dx);
 
   El::DistMatrix<El::BigFloat> dy_dist;
   Zeros(dy_dist, Q.Height(), 1);
+  {
+    El::Matrix<El::BigFloat> dy_sum;
+    Zeros(dy_sum, Q.Height(), 1);
 
-  for(size_t block = 0; block < schur_off_diagonal.blocks.size(); ++block)
-    {
-      Gemv(El::OrientationNS::TRANSPOSE, El::BigFloat(-1),
-           schur_off_diagonal.blocks[block], dx.blocks[block], El::BigFloat(1),
-           dy.blocks[block]);
+    for(size_t block = 0; block < schur_off_diagonal.blocks.size(); ++block)
+      {
+        Gemv(El::OrientationNS::TRANSPOSE, El::BigFloat(-1),
+             schur_off_diagonal.blocks[block], dx.blocks[block],
+             El::BigFloat(1), dy.blocks[block]);
 
-      for(int64_t row = 0; row < dy.blocks[block].LocalHeight(); ++row)
+        // Locally sum contributions to dy
+        for(int64_t row = 0; row < dy.blocks[block].LocalHeight(); ++row)
+          {
+            int64_t global_row(dy.blocks[block].GlobalRow(row));
+            for(int64_t column = 0; column < dy.blocks[block].LocalWidth();
+                ++column)
+              {
+                int64_t global_column(dy.blocks[block].GlobalCol(column));
+                dy_sum(global_row, global_column)
+                  += dy.blocks[block].GetLocal(row, column);
+              }
+          }
+      }
+
+    // Send out updates for dy
+    El::BigFloat zero(0);
+    for(int64_t row = 0; row < dy_sum.Height(); ++row)
+      for(int64_t column = 0; column < dy_sum.Width(); ++column)
         {
-          int64_t global_row(dy.blocks[block].GlobalRow(row));
-          for(int64_t column = 0; column < dy.blocks[block].LocalWidth();
-              ++column)
+          if(dy_sum(row, column)!=zero)
             {
-              int64_t global_column(dy.blocks[block].GlobalCol(column));
-              dy_dist.QueueUpdate(global_row, global_column,
-                                  dy.blocks[block].GetLocal(row, column));
+              dy_dist.QueueUpdate(row, column, dy_sum(row, column));
             }
         }
-    }
+  }
   dy_dist.ProcessQueues();
 
-  // dyExtended = Q^{-1} dyExtended
+  // Set dy_dist to Q^{-1} dy_dist
   El::cholesky::SolveAfter(El::UpperOrLowerNS::UPPER,
                            El::OrientationNS::NORMAL, Q, dy_dist);
   El::DistMatrix<El::BigFloat, El::STAR, El::STAR> dy_local(dy_dist);
