5c8573e06fbe3884ec12547f98f8a5f341aa84ca
Walter Landry <wlandry@caltech.edu>
2019-08-30 11:15:41 -0700

    Add test for unable to write profile

--- test/run_test.sh
+++ test/run_test.sh
@@ -5,13 +5,27 @@
 ./build/pvm2sdp 1024 test/file_list.nsv test/test/
 rm -f test/test.out
 ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test/ > /dev/null
-diff test/test.out test/test.out.orig
+diff test/test_out test/test_out_orig
+result=0
 if [ $? == 0 ]
 then
-    echo "PASS"
-    exit 0
+    echo "PASS Smoke test"
 else
-    echo "FAIL"
-    exit 1
+    echo "FAIL Smoke test"
+    result=1
 fi
     
+mkdir -p test/io_tests
+touch test/io_tests/profile_error.profiling.0
+chmod a-w test/io_tests/profile_error.profiling.0
+mpirun -n 2 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test/ -c test/io_tests/profile_error --verbosity=2 --maxIterations=1 2>/dev/null > /dev/null
+if [ $? != 0 ]
+then
+    echo "PASS Profile Error"
+else
+    echo "FAIL Profile Error"
+    result=1
+fi
+rm -f test/io_tests/profile_error.profiling.0
+
+exit $result
