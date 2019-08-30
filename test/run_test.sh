8436956d8c9c70ac1a9a76833147a3f25f4e0f06
Walter Landry <wlandry@caltech.edu>
2019-08-30 11:20:12 -0700

    Add test for writing bilinear_bases

--- test/run_test.sh
+++ test/run_test.sh
@@ -16,16 +16,29 @@ else
 fi
     
 mkdir -p test/io_tests
+
 touch test/io_tests/profile_error.profiling.0
 chmod a-w test/io_tests/profile_error.profiling.0
 mpirun -n 2 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test/ -c test/io_tests/profile_error --verbosity=2 --maxIterations=1 2>/dev/null > /dev/null
 if [ $? != 0 ]
 then
-    echo "PASS Profile Error"
+    echo "PASS write profile"
 else
-    echo "FAIL Profile Error"
+    echo "FAIL write profile"
     result=1
 fi
 rm -f test/io_tests/profile_error.profiling.0
+
+touch test/io_tests/bilinear_bases.0
+chmod a-w test/io_tests/bilinear_bases.0
+mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests 2>/dev/null > /dev/null
+if [ $? != 0 ]
+then
+    echo "PASS bilinear_bases"
+else
+    echo "FAIL bilinear_bases"
+    result=1
+fi
+rm -f test/io_tests/bilinear_bases.0
 
 exit $result
