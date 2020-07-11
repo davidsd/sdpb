30c075f6b579a14c3850d4a11d8435f1cf1e3b70
Walter Landry <wlandry@caltech.edu>
2020-07-10 20:09:10 -0700

    Halfway convert to generic optimizer

--- src/outer/main.cxx
+++ src/outer/main.cxx
@@ -23,34 +23,22 @@ int main(int argc, char **argv)
   const int64_t precision(1024);
   El::gmp::SetPrecision(precision);
 
+  Functional functional("test/toy_polys", "", false);
+
   const El::BigFloat min_x(0.0), max_x(100.0);
-  std::vector<El::BigFloat> points({min_x}), new_points({max_x});
+  std::vector<El::BigFloat> scale(functional.blocks.size());
+  std::vector<std::vector<El::BigFloat>> weights(functional.blocks.size());
+  for(size_t index(0); index != functional.blocks.size(); ++index)
+    {
+      weights[index].resize(functional.blocks[index].p.size(), 1);
+      scale[index] = Abs(functional.blocks[index].eval(max_x, weights[index]));
+    }
 
-  El::BigFloat f0, f1, scale;
-  eval(min_x, f0, f1);
-  scale = Max(Abs(f0), Abs(f1));
-  eval(max_x, f0, f1);
-  scale = Max(scale, Max(Abs(f0), Abs(f1)));
+  std::vector<std::vector<El::BigFloat>> points(scale.size(), {min_x}),
+    new_points(scale.size(), {max_x});
 
-  // {
-  //   Functional functional("test/toy_polys","",false);
-
-  //   std::vector<std::vector<El::BigFloat>> optimals(1);
-  //   optimals[0].resize(2);
-  //   optimals[0][0]=1;
-  //   optimals[0][1]=1;
-
-  //   std::vector<El::BigFloat> coords(1);
-  //   coords[0]=min_x;
-  //   std::vector<El::BigFloat> result(functional.eval(coords,optimals));
-  //   std::cout << "functional: " << result[0] << "\n";
-  //   coords[0]=max_x;
-  //   std::cout << "functional: " << functional.eval(coords,optimals)[0] << "\n";
-  //   exit(0);
-  // }
-  
   El::BigFloat optimal(0);
-  while(!new_points.empty())
+  while(!new_points.front().empty())
     {
       // 0.01 is completely arbitrary.  We want it big enough to not
       // rule out points that might provide a limit, but not so big to
@@ -59,11 +47,11 @@ int main(int argc, char **argv)
       // For the toy example, this eliminates almost all of the
       // points.  It feels dangerous.
       El::BigFloat tolerance(
-        scale * Sqrt(Sqrt(El::limits::Epsilon<El::BigFloat>())));
+        scale.front() * Sqrt(Sqrt(El::limits::Epsilon<El::BigFloat>())));
       {
         std::vector<El::BigFloat> temp_points;
         El::BigFloat f0, f1;
-        for(auto &point : points)
+        for(auto &point : points.front())
           {
             eval(point, f0, f1);
             if(f0 + optimal * f1 < tolerance)
@@ -71,32 +59,34 @@ int main(int argc, char **argv)
                 temp_points.emplace_back(point);
               }
           }
-        std::swap(points, temp_points);
+        std::swap(points.front(), temp_points);
       }
-      for(auto &point : new_points)
+      for(auto &point : new_points.front())
         {
-          points.emplace_back(point);
+          points.front().emplace_back(point);
         }
-      El::Matrix<El::BigFloat> b(points.size(), 1),
-        A(points.size(), points.size() + 2), c(points.size() + 2, 1);
+      El::Matrix<El::BigFloat> b(points.front().size(), 1),
+        A(points.front().size(), points.front().size() + 2),
+        c(points.front().size() + 2, 1);
       c(0) = 1;
       c(1) = -1;
 
-      for(size_t point(0); point != points.size(); ++point)
+      for(size_t point(0); point != points.front().size(); ++point)
         {
           c(point + 2) = 0;
-          for(size_t index(0); index != points.size(); ++index)
+          for(size_t index(0); index != points.front().size(); ++index)
             {
               A(index, point + 2) = 0;
             }
           A(point, point + 2) = 1;
 
-          eval(points[point], b(point), A(point, 1));
+          eval(points.front()[point], b(point), A(point, 1));
           A(point, 0) = -A(point, 1);
         }
       optimal = solve_LP(A, b, c);
       std::cout.precision(precision / 3.3);
-      std::cout << "solve: " << points.size() << " " << optimal << "\n";
+      std::cout << "solve: " << points.front().size() << " " << optimal
+                << "\n";
       // 0.01 should be a small enough relative error so that we are
       // in the regime of convergence.  Then the error estimates will
       // work
@@ -107,9 +97,9 @@ int main(int argc, char **argv)
                   return f0 + optimal * f1;
                 },
                 0.01);
-      new_points = get_new_points(mesh);
-      // std::cout << "new: " << new_points << "\n";
-      scale = max_value(mesh);
+      new_points.front() = get_new_points(mesh);
+      // std::cout << "new: " << new_points.front() << "\n";
+      scale.front() = max_value(mesh);
     }
   std::cout.precision(precision / 3.3);
   std::cout << "optimal: " << optimal << "\n";
