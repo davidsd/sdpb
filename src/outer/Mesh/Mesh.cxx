df36a8fd89a0008c82341e6f541956d7ff95baa6
Walter Landry <wlandry@caltech.edu>
2020-07-27 21:10:22 -0700

    Stop refining if coordinate differences are too small

--- src/outer/Mesh/Mesh.cxx
+++ src/outer/Mesh/Mesh.cxx
@@ -19,12 +19,19 @@ Mesh::Mesh(const El::BigFloat &x_0, const El::BigFloat &x_2,
     : x({x_0, (x_0 + x_2) / 2, x_2, (x_2 + x_4) / 2, x_4}),
       f({f_0, fn(x[1]), f_2, fn(x[3]), f_4})
 {
+  // Stop refining if we can not really resolve differences in the coordinates
+  if(Abs(x[0] - x[1]) < Sqrt(El::limits::Epsilon<El::BigFloat>()))
+    {
+      return;
+    }
   if(need_refine(f[0], f[1], f[2], epsilon))
     {
-      lower=std::make_unique<Mesh>(x[0], x[1], x[2], f[0], f[1], f[2], fn, epsilon);
+      lower = std::make_unique<Mesh>(x[0], x[1], x[2], f[0], f[1], f[2], fn,
+                                     epsilon);
     }
   if(need_refine(f[2], f[3], f[4], epsilon))
     {
-      upper=std::make_unique<Mesh>(x[2], x[3], x[4], f[2], f[3], f[4], fn, epsilon);
+      upper = std::make_unique<Mesh>(x[2], x[3], x[4], f[2], f[3], f[4], fn,
+                                     epsilon);
     }
 }
