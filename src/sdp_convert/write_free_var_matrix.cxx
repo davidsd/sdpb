ef9eb78cc4c8ef87bf70b55f0325cb0b242c091f
Walter Landry <wlandry@caltech.edu>
2020-12-11 20:51:07 -0800

    Write free_var_matrix_*.json

--- src/sdp_convert/write_free_var_matrix.cxx
+++ src/sdp_convert/write_free_var_matrix.cxx
@@ -14,16 +14,31 @@ void write_free_var_matrix(
       size_t block_size(group.constraint_matrix.Height());
 
       const boost::filesystem::path output_path(
-        output_dir / ("free_var_matrix." + std::to_string(*block_index)));
+        output_dir
+        / ("free_var_matrix_" + std::to_string(*block_index) + ".json"));
       boost::filesystem::ofstream output_stream(output_path);
       set_stream_precision(output_stream);
 
-      output_stream << block_size << " " << dual_objectives_b_size << "\n";
+      output_stream << "[\n";
       for(size_t row = 0; row < block_size; ++row)
-        for(size_t column = 0; column < dual_objectives_b_size; ++column)
-          {
-            output_stream << group.constraint_matrix(row, column) << "\n";
-          }
+        {
+          if(row != 0)
+            {
+              output_stream << ",\n";
+            }
+          output_stream << "  [\n";
+          for(size_t column = 0; column < dual_objectives_b_size; ++column)
+            {
+              if(column != 0)
+                {
+                  output_stream << ",\n";
+                }
+              output_stream << "    \"" << group.constraint_matrix(row, column)
+                            << "\"";
+            }
+          output_stream << "\n  ]";
+        }
+      output_stream << "\n]\n";
       if(!output_stream.good())
         {
           throw std::runtime_error("Error when writing to: "
