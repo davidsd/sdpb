ca956b350b875246e4880983be900abeb2e6c52a
Walter Landry <wlandry@caltech.edu>
2020-12-12 22:29:41 -0800

    Write bilinear_bases by block, not rank

--- src/sdp_convert/write_bilinear_bases.cxx
+++ src/sdp_convert/write_bilinear_bases.cxx
@@ -6,29 +6,24 @@
 #include <vector>
 
 void write_bilinear_bases(
-  const boost::filesystem::path &output_dir, const int &rank,
+  const boost::filesystem::path &output_dir,
   const std::vector<size_t> &indices,
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups)
 {
-  const boost::filesystem::path output_path(
-    output_dir / ("bilinear_bases_" + std::to_string(rank) + ".json"));
-  boost::filesystem::ofstream output_stream(output_path);
-  set_stream_precision(output_stream);
-  output_stream << "[\n";
   for(size_t block(0); block != dual_constraint_groups.size(); ++block)
     {
-      if(block != 0)
-        {
-          output_stream << ",\n";
-        }
-      output_stream << "  {\n    \"block_index\": " << indices.at(block)
-                    << ",\n    \"bilinear_bases_even\":\n    [\n";
+      const boost::filesystem::path output_path(
+        output_dir
+        / ("bilinear_bases_" + std::to_string(indices.at(block)) + ".json"));
+      boost::filesystem::ofstream output_stream(output_path);
+      set_stream_precision(output_stream);
+      output_stream << "{\n  \"even\":\n  [\n";
       for(auto basis(dual_constraint_groups[block].bilinear_bases.begin());
           basis != dual_constraint_groups[block].bilinear_bases.end(); ++basis)
         {
           if(basis != dual_constraint_groups[block].bilinear_bases.begin())
             {
-              output_stream << ",\n    \"bilinear_bases_odd\":\n    [\n";
+              output_stream << ",\n  \"odd\":\n  [\n";
             }
           // Ensure that each bilinearBasis is sampled the correct number
           // of times
@@ -40,26 +35,24 @@ void write_bilinear_bases(
                 {
                   output_stream << ",\n";
                 }
-              output_stream << "      [\n";
+              output_stream << "    [\n";
               for(int64_t column = 0; column < basis->Width(); ++column)
                 {
                   if(column != 0)
                     {
                       output_stream << ",\n";
                     }
-                  output_stream << "        \"" << (*basis)(row, column)
-                                << "\"";
+                  output_stream << "      \"" << (*basis)(row, column) << "\"";
                 }
-              output_stream << "\n      ]";
+              output_stream << "\n    ]";
             }
-          output_stream << "\n    ]";
+          output_stream << "\n  ]";
         }
-      output_stream << "\n  }";
-    }
-  output_stream << "\n]\n";
-  if(!output_stream.good())
-    {
-      throw std::runtime_error("Error when writing to: "
-                               + output_path.string());
+      output_stream << "\n}\n";
+      if(!output_stream.good())
+        {
+          throw std::runtime_error("Error when writing to: "
+                                   + output_path.string());
+        }
     }
 }
