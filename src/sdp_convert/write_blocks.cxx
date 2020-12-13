21f66dba9822847895af78810e8db6a049b5fe27
Walter Landry <wlandry@caltech.edu>
2020-12-12 22:34:33 -0800

    Write blocks_*.json by block, not rank

--- src/sdp_convert/write_blocks.cxx
+++ src/sdp_convert/write_blocks.cxx
@@ -2,31 +2,24 @@
 #include "write_vector.hxx"
 
 void write_blocks(
-  const boost::filesystem::path &output_dir, const int &rank,
+  const boost::filesystem::path &output_dir,
   const std::vector<size_t> &indices,
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups)
 {
-  const boost::filesystem::path output_path(
-    output_dir / ("blocks_" + std::to_string(rank) + ".json"));
-  boost::filesystem::ofstream output_stream(output_path);
-  output_stream << "{\n  \"blocks\": [\n";
-  for(size_t block(0); block != indices.size(); ++block)
+  for(size_t block(0); block != dual_constraint_groups.size(); ++block)
     {
-      if(block != 0)
-        {
-          output_stream << ",\n";
-        }
-      output_stream << "    {\n      \"index\": " << indices.at(block)
-                    << ",\n      \"dim\": "
-                    << dual_constraint_groups.at(block).dim
-                    << ",\n      \"num_points\": "
+      const boost::filesystem::path output_path(
+        output_dir
+        / ("blocks_" + std::to_string(indices.at(block)) + ".json"));
+      boost::filesystem::ofstream output_stream(output_path);
+      output_stream << "{\n  \"dim\": " << dual_constraint_groups.at(block).dim
+                    << ",\n  \"num_points\": "
                     << (dual_constraint_groups.at(block).degree + 1)
-                    << "\n    }";
-    }
-  output_stream << "\n  ]\n}\n";
-  if(!output_stream.good())
-    {
-      throw std::runtime_error("Error when writing to: "
-                               + output_path.string());
+                    << "\n}\n";
+      if(!output_stream.good())
+        {
+          throw std::runtime_error("Error when writing to: "
+                                   + output_path.string());
+        }
     }
 }
--- src/sdp_convert/write_sdpb_input_files.cxx
+++ src/sdp_convert/write_sdpb_input_files.cxx
@@ -18,7 +18,7 @@ void write_bilinear_bases(
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups);
 
 void write_blocks(
-  const boost::filesystem::path &output_dir, const int &rank,
+  const boost::filesystem::path &output_dir,
   const std::vector<size_t> &indices,
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups);
 
@@ -46,7 +46,7 @@ void write_sdpb_input_files(
       write_objectives(output_dir, objective_const, dual_objective_b);
     }
   write_bilinear_bases(output_dir, indices, dual_constraint_groups);
-  write_blocks(output_dir, rank, indices, dual_constraint_groups);
+  write_blocks(output_dir, indices, dual_constraint_groups);
   write_primal_objective_c(output_dir, indices, dual_constraint_groups);
   write_free_var_matrix(output_dir, indices, dual_objective_b.size(),
                         dual_constraint_groups);
