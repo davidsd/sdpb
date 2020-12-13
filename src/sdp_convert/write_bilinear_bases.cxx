81fc447671840f8b6f0c72fb517b36d66e4af31c
Walter Landry <wlandry@caltech.edu>
2020-12-12 22:12:32 -0800

    Make bilinear_bases_*.json more self-describing

--- src/sdp_convert/write_bilinear_bases.cxx
+++ src/sdp_convert/write_bilinear_bases.cxx
@@ -7,55 +7,56 @@
 
 void write_bilinear_bases(
   const boost::filesystem::path &output_dir, const int &rank,
+  const std::vector<size_t> &indices,
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups)
 {
   const boost::filesystem::path output_path(
     output_dir / ("bilinear_bases_" + std::to_string(rank) + ".json"));
   boost::filesystem::ofstream output_stream(output_path);
   set_stream_precision(output_stream);
-  output_stream << "{\n  \"dual_constraints\": [\n";
-  for(auto group(dual_constraint_groups.begin());
-      group != dual_constraint_groups.end(); ++group)
+  output_stream << "[\n";
+  for(size_t block(0); block != dual_constraint_groups.size(); ++block)
     {
-      if(group != dual_constraint_groups.begin())
+      if(block != 0)
         {
           output_stream << ",\n";
         }
-      output_stream << "    {\n      \"bilinear_bases\": [\n";
-      for(auto basis(group->bilinear_bases.begin());
-          basis != group->bilinear_bases.end(); ++basis)
+      output_stream << "  {\n    \"block_index\": " << indices.at(block)
+                    << ",\n    \"bilinear_bases_even\":\n    [\n";
+      for(auto basis(dual_constraint_groups[block].bilinear_bases.begin());
+          basis != dual_constraint_groups[block].bilinear_bases.end(); ++basis)
         {
-          if(basis != group->bilinear_bases.begin())
+          if(basis != dual_constraint_groups[block].bilinear_bases.begin())
             {
-              output_stream << ",\n";
+              output_stream << ",\n    \"bilinear_bases_odd\":\n    [\n";
             }
           // Ensure that each bilinearBasis is sampled the correct number
           // of times
-          assert(static_cast<size_t>(basis->Width()) == group->degree + 1);
-          output_stream << "        [\n";
+          assert(static_cast<size_t>(basis->Width())
+                 == dual_constraint_groups[block].degree + 1);
           for(int64_t row = 0; row < basis->Height(); ++row)
             {
               if(row != 0)
                 {
                   output_stream << ",\n";
                 }
-              output_stream << "          [\n";
+              output_stream << "      [\n";
               for(int64_t column = 0; column < basis->Width(); ++column)
                 {
                   if(column != 0)
                     {
                       output_stream << ",\n";
                     }
-                  output_stream << "            \"" << (*basis)(row, column)
+                  output_stream << "        \"" << (*basis)(row, column)
                                 << "\"";
                 }
-              output_stream << "\n          ]";
+              output_stream << "\n      ]";
             }
-          output_stream << "\n        ]";
+          output_stream << "\n    ]";
         }
-      output_stream << "\n      ]\n    }";
+      output_stream << "\n  }";
     }
-  output_stream << "\n  ]\n}\n";
+  output_stream << "\n]\n";
   if(!output_stream.good())
     {
       throw std::runtime_error("Error when writing to: "
--- src/sdp_convert/write_sdpb_input_files.cxx
+++ src/sdp_convert/write_sdpb_input_files.cxx
@@ -14,6 +14,7 @@ void write_objectives(const boost::filesystem::path &output_dir,
 
 void write_bilinear_bases(
   const boost::filesystem::path &output_dir, const int &rank,
+  const std::vector<size_t> &indices,
   const std::vector<Dual_Constraint_Group> &dual_constraint_groups);
 
 void write_blocks(
@@ -44,7 +45,7 @@ void write_sdpb_input_files(
       write_control(output_dir, num_procs, command_arguments);
       write_objectives(output_dir, objective_const, dual_objective_b);
     }
-  write_bilinear_bases(output_dir, rank, dual_constraint_groups);
+  write_bilinear_bases(output_dir, rank, indices, dual_constraint_groups);
   write_blocks(output_dir, rank, indices, dual_constraint_groups);
   write_primal_objective_c(output_dir, indices, dual_constraint_groups);
   write_free_var_matrix(output_dir, indices, dual_objective_b.size(),
