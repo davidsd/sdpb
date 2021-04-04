f49e0c5f3b18f8f050f860fa7289b52f08a75711
Walter Landry <wlandry@caltech.edu>
2021-04-04 14:30:57 -0700

    Only fill matrices if needed in sdp2input.  Saves significant memory.

--- src/sdp_read/read_input/read_json/read_json.cxx
+++ src/sdp_read/read_input/read_json/read_json.cxx
@@ -26,8 +26,14 @@ void read_json(const boost::filesystem::path &input_path,
   auto &temp_matrices(
     parser.positive_matrices_with_prefactor_state.value);
   matrices.resize(matrices.size() + temp_matrices.size());
+  
+  size_t rank(El::mpi::Rank(El::mpi::COMM_WORLD)),
+    num_procs(El::mpi::Size(El::mpi::COMM_WORLD));
   for(size_t index = 0; index < temp_matrices.size(); ++index)
     {
-      std::swap(matrices[offset + index], temp_matrices[index]);
+      if((offset + index) % num_procs == rank)
+        {
+          std::swap(matrices[offset + index], temp_matrices[index]);
+        }
     }
 }
