05238efc3525faeb228a21840bdc163203f8cdb3
Walter Landry <wlandry@caltech.edu>
2021-04-04 09:28:34 -0700

    Fix Output_Parameters serializaton into a property tree

--- src/outer_limits/Outer_Parameters/to_property_tree.cxx
+++ src/outer_limits/Outer_Parameters/to_property_tree.cxx
@@ -4,8 +4,9 @@ boost::property_tree::ptree to_property_tree(const Outer_Parameters &p)
 {
   boost::property_tree::ptree result(to_property_tree(p.solver));
 
-  result.put("sdp", p.functions_path.string());
-  result.put("outDir", p.output_path.string());
+  result.put("functions", p.functions_path.string());
+  result.put("points", p.points_path.string());
+  result.put("out", p.output_path.string());
   result.put("noFinalCheckpoint", p.no_final_checkpoint);
   result.put("dualityGapReduction", p.duality_gap_reduction);
   result.put("writeSolution", p.write_solution);
