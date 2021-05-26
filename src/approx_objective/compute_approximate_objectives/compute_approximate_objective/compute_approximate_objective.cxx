0f51a24a8b7234489f6466ba12309175b3ef4dc8
Walter Landry <wlandry@caltech.edu>
2021-05-26 12:22:37 -0700

    Compute 1st and 2nd order parts separately.
    Slightly different answers because we have different 3rd order errors.
    
    Converges to cubic order with TTTT_small.

--- src/approx_objective/compute_approximate_objectives/compute_approximate_objective/compute_approximate_objective.cxx
+++ src/approx_objective/compute_approximate_objectives/compute_approximate_objective/compute_approximate_objective.cxx
@@ -20,40 +20,60 @@ El::BigFloat compute_approximate_objective(
 
   // b.y
   El::BigFloat objective(El::Dot(sdp.dual_objective_b, y.blocks.at(0)));
-  // ydy = y + dy/2
-  El::DistMatrix<El::BigFloat> ydy(y.blocks.at(0));
-  El::Axpy(El::BigFloat(0.5), dy.blocks.at(0), ydy);
 
-  // db.(y + dy/2)
-  objective += El::Dot(d_sdp.dual_objective_b, ydy);
+  El::BigFloat linear(0), quad(0);
+  // El::BigFloat first(0), second(0);
+  // db.y
+  objective += El::Dot(d_sdp.dual_objective_b, y.blocks.at(0));
 
+  linear+=El::Dot(d_sdp.dual_objective_b, y.blocks.at(0));
+  
+  // db.dy/2
+  objective += El::Dot(d_sdp.dual_objective_b, dy.blocks.at(0))/2;
+
+  quad += El::Dot(d_sdp.dual_objective_b, dy.blocks.at(0))/2;
+  
   El::BigFloat local_sum(0);
   for(size_t block(0); block != x.blocks.size(); ++block)
     {
       // dc.x
       local_sum
         += Dotu(d_sdp.primal_objective_c.blocks.at(block), x.blocks.at(block));
+
+      linear+=Dotu(d_sdp.primal_objective_c.blocks.at(block), x.blocks.at(block));
+      
       // dc.dx/2
       local_sum
-        += Dotu(d_sdp.primal_objective_c.blocks.at(block), dx.blocks.at(block))
-           / 2;
+        += Dotu(d_sdp.primal_objective_c.blocks.at(block), dx.blocks.at(block))/2;
+
+      quad += Dotu(d_sdp.primal_objective_c.blocks.at(block), dx.blocks.at(block))/2;
       {
-        // temp = dB.(y + dy/2)
+        // temp = dB.y
         El::DistMatrix<El::BigFloat> temp(x.blocks.at(block));
         El::Zero(temp);
         El::Gemv(El::Orientation::NORMAL, El::BigFloat(1.0),
-                 d_sdp.free_var_matrix.blocks[block], ydy, El::BigFloat(0.0),
+                 d_sdp.free_var_matrix.blocks[block], y.blocks.at(0), El::BigFloat(0.0),
                  temp);
 
-        // xdx = x + dx/2
-        El::DistMatrix<El::BigFloat> xdx(x.blocks[block]);
-        El::Axpy(El::BigFloat(0.5), dx.blocks[block], xdx);
+        // -x.dB.y/
+        local_sum -= El::Dotu(temp, x.blocks.at(block));
 
-        // -(x + dx/2).dB.(y + dy/2)
-        // Doing it this way reduces the number of dot products, but
-        // includes the incorrect third order term dx.dB.dy/4 which
-        // comes along for the ride.
-        local_sum -= El::Dotu(temp, xdx);
+        linear -= El::Dotu(temp, x.blocks.at(block));
+        
+        // -dx.dB.y/2
+        local_sum -= El::Dotu(temp, dx.blocks.at(block))/2;
+
+        quad -= El::Dotu(temp, dx.blocks.at(block))/2;
+        
+        // temp = dB.dy
+        El::Gemv(El::Orientation::NORMAL, El::BigFloat(1.0),
+                 d_sdp.free_var_matrix.blocks[block], dy.blocks.at(0),
+                 El::BigFloat(0.0), temp);
+
+        // -x.dB.dy/2
+        local_sum -= El::Dotu(temp, x.blocks.at(block))/2;
+        
+        quad -= El::Dotu(temp, x.blocks.at(block))/2;
       }
     }
   if(!x.blocks.empty() && x.blocks.at(0).Grid().Rank() != 0)
