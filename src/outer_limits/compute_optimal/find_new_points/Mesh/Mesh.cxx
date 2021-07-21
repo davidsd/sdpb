86ae3783ea9a7bdfda19022f8719dd0d99588932
Walter Landry <wlandry@caltech.edu>
2021-07-20 20:16:46 -0700

    Remove commented code

--- src/outer_limits/compute_optimal/find_new_points/Mesh/Mesh.cxx
+++ src/outer_limits/compute_optimal/find_new_points/Mesh/Mesh.cxx
@@ -23,23 +23,6 @@ Mesh::Mesh(const El::BigFloat &x_0, const El::BigFloat &x_2,
     : x({x_0, (x_0 + x_2) / 2, x_2, (x_2 + x_4) / 2, x_4}),
       f({f_0, fn(x[1]), f_2, fn(x[3]), f_4})
 {
-  // if(El::mpi::Rank() == 2)
-  //   {
-  //     std::cout.precision(15);
-  //     std::cout << "Mesh: "
-  //               << x[0] << " "
-  //               << x[4] << " "
-  //               << Abs(x[0] - x[1]) << " "
-  //               << f[0] << " "
-  //               << f[1] << " "
-  //               << f[2] << " "
-  //               << f[3] << " "
-  //               << f[4] << " "
-  //               << El::Abs(f[0]-f[4]) << " "
-  //               << Sqrt(El::limits::Epsilon<El::BigFloat>()) << " "
-  //               << "\n";
-  //   }
-
   // Stop refining if we can not really resolve differences in the coordinates
   // TODO: Scale this by max_delta
   if(Abs(x[0] - x[1]) < Sqrt(El::limits::Epsilon<El::BigFloat>()))
