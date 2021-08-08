22b11046a8146279f4f3964446fb68652ccad139
Walter Landry <wlandry@caltech.edu>
2021-08-08 15:40:49 -0700

    Add sdp2functions

---
+++ src/sdp2functions/read_input_files/read_input_files.cxx
@@ -0,0 +1,39 @@
+#include "../../sdp_read.hxx"
+#include "../../sdp_convert.hxx"
+
+void read_xml_input(
+  const boost::filesystem::path &input_file,
+  std::vector<El::BigFloat> &dual_objectives_b,
+  std::vector<Polynomial_Vector_Matrix> &polynomial_vector_matrices,
+  size_t &num_processed);
+
+void read_input_files(
+  const std::vector<boost::filesystem::path> &input_files,
+  std::vector<El::BigFloat> &dual_objectives_b,
+  std::vector<Polynomial_Vector_Matrix> &polynomial_vector_matrices,
+  size_t &num_processed);
+
+void read_input_files(
+  const std::vector<boost::filesystem::path> &input_files,
+  std::vector<El::BigFloat> &dual_objectives_b,
+  std::vector<Polynomial_Vector_Matrix> &polynomial_vector_matrices,
+  size_t &num_processed)
+{
+  for(auto &input_file : input_files)
+    {
+      if(input_file.empty())
+        {
+          continue;
+        }
+      if(input_file.extension() == ".nsv")
+        {
+          read_input_files(read_file_list(input_file), dual_objectives_b,
+                           polynomial_vector_matrices, num_processed);
+        }
+      else
+        {
+          read_xml_input(input_file, dual_objectives_b,
+                         polynomial_vector_matrices, num_processed);
+        }
+    }
+}
--- wscript
+++ wscript
@@ -132,6 +132,19 @@ def build(bld):
                 use=use_packages + ['sdp_read']
                 )
 
+    bld.program(source=['src/sdp2functions/main.cxx',
+                        'src/sdp2functions/parse_command_line.cxx',
+                        'src/sdp2functions/read_input_files/read_input_files.cxx',
+                        'src/sdp2functions/read_input_files/read_xml_input/read_xml_input.cxx',
+                        'src/sdp2functions/read_input_files/read_xml_input/Input_Parser/on_start_element.cxx',
+                        'src/sdp2functions/read_input_files/read_xml_input/Input_Parser/on_end_element.cxx',
+                        'src/sdp2functions/read_input_files/read_xml_input/Input_Parser/on_characters.cxx',
+                        'src/sdp2functions/write_functions/write_functions.cxx'],
+                target='sdp2functions',
+                cxxflags=default_flags,
+                use=use_packages + ['sdp_read']
+                )
+
     sdp_read_sources=['src/sdp_read/read_input/read_input.cxx',
                       'src/sdp_read/read_input/read_json/read_json.cxx',
                       'src/sdp_read/read_input/read_json/Positive_Matrix_With_Prefactor_State/json_key.cxx',
