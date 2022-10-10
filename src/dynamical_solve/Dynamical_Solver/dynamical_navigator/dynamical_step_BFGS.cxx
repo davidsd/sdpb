b749231d95958776618818b00ea5e7a6f001842d
Aike Liu <aliu7@caltech.edu>
2022-10-09 17:03:49 -0700

    merge cleaned up code

--- src/dynamical_solve/Dynamical_Solver/dynamical_navigator/dynamical_step_BFGS.cxx
+++ src/dynamical_solve/Dynamical_Solver/dynamical_navigator/dynamical_step_BFGS.cxx
@@ -475,6 +475,7 @@ void Dynamical_Solver::dynamical_step(
 	El::Matrix<El::BigFloat> grad_mixed(n_external_parameters, 1); //H_px (-internal_dx_dy) = H_px (H^-1_xx Del_x L_mu) in Eq (13).  
 
 	El::Matrix<El::BigFloat> prev_grad(n_external_parameters, 1), prev_step(n_external_parameters, 1), prev_BFGS(n_external_parameters, n_external_parameters);
+        El::Zeros(hess_BFGS, n_external_parameters,n_external_parameters);
 	read_prev_grad_step_hess(dynamical_parameters, prev_grad, prev_step, prev_BFGS);
 
 	read_sdp_grid(dynamical_parameters, block_info, sdp, grid, timers,
