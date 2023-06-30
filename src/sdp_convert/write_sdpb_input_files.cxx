40ec237d7c428f78ab1fd319a33908ee716be7c5
Vasiliy Dommes <vasdommes@gmail.com>
2023-06-29 23:56:33 -0300

    sdp2input: print matrix sizes and RAM estimates (debug mode) TODO: same for pvm2sdp
    Related: #76 Print out info about the sdp at startup

--- src/sdp_convert/write_sdpb_input_files.cxx
+++ src/sdp_convert/write_sdpb_input_files.cxx
@@ -30,6 +30,10 @@ void write_free_var_matrix(std::ostream &output_stream,
                            const size_t &dual_objectives_b_size,
                            const Dual_Constraint_Group &group);
 
+void print_matrix_sizes(
+  const int &rank, const std::vector<El::BigFloat> &dual_objective_b,
+  const std::vector<Dual_Constraint_Group> &dual_constraint_groups);
+
 namespace
 {
   void archive_gzipped_file(const boost::filesystem::path &path,
@@ -48,7 +52,8 @@ void write_sdpb_input_files(
   const size_t &num_blocks, const std::vector<std::string> &command_arguments,
   const El::BigFloat &objective_const,
   const std::vector<El::BigFloat> &dual_objective_b,
-  const std::vector<Dual_Constraint_Group> &dual_constraint_groups)
+  const std::vector<Dual_Constraint_Group> &dual_constraint_groups,
+  const bool debug)
 {
   boost::filesystem::path temp_dir(output_path);
   temp_dir += "_temp";
@@ -96,6 +101,10 @@ void write_sdpb_input_files(
     }
   El::mpi::Reduce(block_file_sizes.data(), block_file_sizes.size(),
                   El::mpi::SUM, 0, El::mpi::COMM_WORLD);
+  if(debug)
+    {
+      print_matrix_sizes(rank, dual_objective_b, dual_constraint_groups);
+    }
   if(rank == 0)
     {
       Archive_Writer writer(output_path);
@@ -117,5 +126,110 @@ void write_sdpb_input_files(
           boost::filesystem::remove(block_path);
         }
       boost::filesystem::remove(temp_dir);
+    }
+}
+
+void print_matrix_sizes(
+  const int &rank, const std::vector<El::BigFloat> &dual_objective_b,
+  const std::vector<Dual_Constraint_Group> &dual_constraint_groups)
+{
+  if(rank == 0)
+    {
+      El::Output("---------------------");
+      El::Output("Matrix sizes and RAM estimates:");
+      El::Output("---------------------");
+    }
+
+  size_t N = dual_objective_b.size();
+  size_t my_P = 0; // P' for a given group
+
+  // Hereafter XXX_elements means number of nonzero elements in XXX
+  size_t my_constraint_matrix_elements = 0;
+  size_t my_bilinear_bases_elements = 0;
+  size_t my_bilinear_pairing_block_elements = 0;
+  size_t my_psd_elements = 0;
+  size_t my_schur_elements = 0;
+  for(auto &group : dual_constraint_groups)
+    {
+      my_P += group.constraint_constants.size();
+
+      my_constraint_matrix_elements += group.constraint_matrix.MemorySize();
+      my_bilinear_bases_elements += group.bilinear_bases[0].MemorySize()
+                                    + group.bilinear_bases[1].MemorySize();
+
+      // variables stored in Block_Info
+      size_t dimensions = group.dim;
+      size_t num_points = group.degree + 1; // see write_blocks()
+
+      // See Block_Info::schur_block_sizes()
+      size_t schur_width = num_points * dimensions * (dimensions + 1) / 2;
+      my_schur_elements += schur_width * schur_width;
+
+      // See Block_Info::bilinear_pairing_block_sizes()
+      // two blocks, each one has same size
+      size_t bilinear_pairing_block_width = num_points * dimensions;
+      my_bilinear_pairing_block_elements
+        += 2 * bilinear_pairing_block_width * bilinear_pairing_block_width;
+
+      // See Block_Info::psd_matrix_block_sizes()
+      // for each group we have two psd square matrix blocks, with width
+      // psd_even and psd_odd
+      auto psd_even = dimensions * ((num_points + 1) / 2);
+      auto psd_odd = dimensions * num_points - psd_even;
+      my_psd_elements += psd_even * psd_even + psd_odd * psd_odd;
+    }
+  if(my_P * N != my_constraint_matrix_elements)
+    {
+      El::LogicError("sum(P'*N) != #(B bands): P'=", my_P, ", N=", N,
+                     "#(B bands)=", my_constraint_matrix_elements);
+    }
+
+  // NB: Reduce should be called on all ranks, its result used only on rank 0!
+  auto reduce_sum = [](size_t item) {
+    return El::mpi::Reduce(item, El::mpi::SUM, 0, El::mpi::COMM_WORLD);
+  };
+
+  size_t B_matrix_elements = reduce_sum(my_constraint_matrix_elements);
+  size_t bilinear_pairing_block_elements
+    = reduce_sum(my_bilinear_pairing_block_elements);
+  size_t psd_blocks_elements = reduce_sum(my_psd_elements);
+  size_t schur_elements = reduce_sum(my_schur_elements);
+  size_t P = reduce_sum(my_P);
+  size_t bilinear_bases_elements = reduce_sum(my_bilinear_bases_elements);
+
+  if(rank == 0)
+    {
+      size_t big_float_bytes = El::BigFloat(1.1).SerializedSize();
+
+      size_t Q_matrix_size = N * N;
+      size_t total_no_Q = 2 * B_matrix_elements + 5 * psd_blocks_elements
+                          + 2 * schur_elements
+                          + 2 * bilinear_pairing_block_elements;
+
+      std::vector<std::pair<std::string, size_t>> sizes{
+        {"P (primal objective)", P},
+        {"N (dual objective)", N},
+        {"B matrix (PxN)", B_matrix_elements},
+        {"Q matrix (NxN)", Q_matrix_size},
+        {"Bilinear bases", bilinear_bases_elements},
+        {"Bilinear pairing blocks", bilinear_pairing_block_elements},
+        {"PSD blocks", psd_blocks_elements},
+        {"Schur (PxP block diagonal)", schur_elements},
+        {"Total (no Q) = 2#(B) + 5#(PSD) + 2#(S) + 2#(Bilinear pairing)",
+         total_no_Q},
+      };
+
+      El::Output("BigFloat, bytes: ", big_float_bytes);
+      for(auto &[key, value] : sizes)
+        {
+          El::Output(key, ", elements: ", value);
+          El::Output(key, ", bytes: ", value * big_float_bytes);
+        }
+      double GB_per_element = (double)big_float_bytes / 1024 / 1024 / 1024;
+      El::Output("Total RAM (no Q), GB: ", total_no_Q * GB_per_element);
+      El::Output("Q, GB: ", Q_matrix_size * GB_per_element);
+      El::Output("NB: Q is copied over each core group, i.e. "
+                 "(nodes*procsPerNode/procGranularity) times");
+      El::Output("---------------------");
     }
 }
