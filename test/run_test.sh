dc0f04e19033581a9721fd09abba5d75661b2d5b
Vasiliy Dommes <vasdommes@gmail.com>
2023-07-10 11:35:23 -0700

    run_test.sh: extract test_run_success and test_run_fails functions

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,187 +3,114 @@
 # Run this from the top level directory
 result=0
 
-rm -rf test/test/
-./build/pvm2sdp 1024 test/file_list.nsv test/test
-if [ $? == 0 ]
-then
-    echo "PASS pvm2sdp"
-else
-    echo "FAIL pvm2sdp"
+function test_run_with_exit_code() {
+  local expected_exit_code=${1}
+  local name=${2}
+  local cmd=${*:3}
+  if [ $expected_exit_code == 0 ]; then
+    $cmd >/dev/null
+  else
+    $cmd >/dev/null 2>/dev/null
+  fi
+  local cmd_exit_code=$?
+  if [ $cmd_exit_code == $expected_exit_code ]; then
+    echo "PASS $name"
+  else
+    echo "FAIL $name: exit code $cmd_exit_code, expected $expected_exit_code"
     result=1
-fi
+  fi
+}
+
+function test_run_success() {
+  test_run_with_exit_code 0 "$@"
+}
+
+function test_run_fails() {
+  test_run_with_exit_code 1 "$@"
+}
+
+rm -rf test/test/
+test_run_success "pvm2sdp" ./build/pvm2sdp 1024 test/file_list.nsv test/test
 
 rm -f test/test.out
-./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test --verbosity=0
-diff test/test_out test/test_out_orig
-if [ $? == 0 ]
-then
-    echo "PASS SDPB"
-else
-    echo "FAIL SDPB"
-    result=1
-fi
+test_run_success "SDPB" ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 touch test/io_tests/profile_error.profiling.0
 chmod a-w test/io_tests/profile_error.profiling.0
-mpirun -n 2 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/profile_error --verbosity=2 --maxIterations=1 2>/dev/null > /dev/null
-if [ $? != 0 ]
-then
-    echo "PASS write profile"
-else
-    echo "FAIL write profile"
-    result=1
-fi
+test_run_fails "write profile" mpirun -n 2 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/profile_error --verbosity=2 --maxIterations=1
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 touch test/io_tests/control.json
 chmod a-w test/io_tests/control.json
-mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS control"
-else
-    echo "FAIL control"
-    result=1
-fi
+test_run_fails "control" mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 touch test/io_tests/block_0.json
 chmod a-w test/io_tests/block_0.json
-mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS blocks write"
-else
-    echo "FAIL blocks write"
-    result=1
-fi
+test_run_fails "blocks write" mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 touch test/io_tests/objectives.json
 chmod a-w test/io_tests/objectives.json
-mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS objectives"
-else
-    echo "FAIL objectives"
-    result=1
-fi
+test_run_fails "objectives" mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/test.xml test/io_tests
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 mkdir test/io_tests/out
 touch test/io_tests/out/out.txt
 chmod a-w test/io_tests/out/out.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS out.txt"
-else
-    echo "FAIL out.txt"
-    result=1
-fi
+test_run_fails "out.txt" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 mkdir test/io_tests/out
 touch test/io_tests/out/x_0.txt
 chmod a-w test/io_tests/out/x_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS x_0.txt"
-else
-    echo "FAIL x_0.txt"
-    result=1
-fi
+test_run_fails "x_0.txt" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 mkdir test/io_tests/out
 touch test/io_tests/out/y.txt
 chmod a-w test/io_tests/out/y.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS y.txt"
-else
-    echo "FAIL y.txt"
-    result=1
-fi
+test_run_fails "y.txt" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 mkdir test/io_tests/out
 touch test/io_tests/out/X_matrix_0.txt
 chmod a-w test/io_tests/out/X_matrix_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --writeSolution=x,y,X,Y --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS X_matrix_0.txt"
-else
-    echo "FAIL X_matrix_0.txt"
-    result=1
-fi
+test_run_fails "X_matrix_0.txt" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --writeSolution=x,y,X,Y --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 mkdir test/io_tests/out
 touch test/io_tests/out/Y_matrix_0.txt
 chmod a-w test/io_tests/out/Y_matrix_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --writeSolution=x,y,X,Y --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS Y_matrix_0.txt"
-else
-    echo "FAIL Y_matrix_0.txt"
-    result=1
-fi
+test_run_fails "Y_matrix_0.txt" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --writeSolution=x,y,X,Y --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
-echo "file_does_not_exist" > test/io_tests/file_list.nsv
-mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/io_tests/file_list.nsv test/io_tests 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS file_list.nsv"
-else
-    echo "FAIL file_list.nsv"
-    result=1
-fi
+echo "file_does_not_exist" >test/io_tests/file_list.nsv
+test_run_fails "file_list.nsv" mpirun -n 1 --quiet ./build/pvm2sdp 1024 test/io_tests/file_list.nsv test/io_tests
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 cp -r test/test test/io_tests
 perl -p -0777 -i -e 'substr($_,1138,1)^=chr(1<<5)' test/io_tests/test
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS input corruption"
-else
-    echo "FAIL input corruption"
-    result=1
-fi
+test_run_fails "input corruption" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 #
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 cp -r test/test test/io_tests
 mpirun -n 1 ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 --writeSolution=x,y,X,Y
 chmod a-r test/io_tests/out/X_matrix_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS text checkpoint read"
-else
-    echo "FAIL text checkpoint read"
-    result=1
-fi
+test_run_fails "text checkpoint read" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0 #
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
@@ -191,28 +118,14 @@ cp -r test/test test/io_tests
 mpirun -n 1 ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 --writeSolution=x,y,X,Y
 rm test/io_tests/out/X_matrix_0.txt
 touch test/io_tests/out/X_matrix_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS text checkpoint header"
-else
-    echo "FAIL text checkpoint header"
-    result=1
-fi
+test_run_fails "text checkpoint header" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0
 rm -rf test/io_tests
 
 mkdir -p test/io_tests
 cp -r test/test test/io_tests
 mpirun -n 1 ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/ck -o test/io_tests/out --maxIterations=1 --verbosity=0 --writeSolution=x,y,X,Y
-head -n 2 test/io_tests/out/X_matrix_0.txt > test/io_tests/out/X_matrix_0.txt
-mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0 2>/dev/null
-if [ $? != 0 ]
-then
-    echo "PASS text checkpoint data"
-else
-    echo "FAIL text checkpoint data"
-    result=1
-fi
+head -n 2 test/io_tests/out/X_matrix_0.txt >test/io_tests/out/X_matrix_0.txt
+test_run_fails "text checkpoint data" mpirun -n 1 --quiet ./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/io_tests/test -c test/io_tests/out -o test/io_tests/out_new --maxIterations=1 --verbosity=0
 rm -rf test/io_tests
 
 exit $result
