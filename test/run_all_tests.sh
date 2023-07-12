0de6baa911546799059d9b0785228350668199e3
Vasiliy Dommes <vasdommes@gmail.com>
2023-07-11 20:55:18 -0700

    Fix #85 add sdp2input tests

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -8,6 +8,7 @@ cd .. # sdpb/
 # setup
 source test/common_test_setup.sh || { echo "Run this script from sdpb root directory"; exit 1; }
 
+source test/sdp2input_test.sh
 source test/sdpb_test.sh
 source test/outer_test.sh
 source test/spectrum_test.sh
