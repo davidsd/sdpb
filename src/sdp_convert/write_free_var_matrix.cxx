1d1aa29fbd42d112eeca801ac6180dd73c2a1444
Vasiliy Dommes <vasdommes@gmail.com>
2023-07-18 15:11:47 -0400

    write_free_var_matrix(): use matrix.Width() instead of dual_objective_b_size
    also assert that they are equal
    
    (cherry picked from commit 6d09479e72ea842f8f7a1399a7846dbb8d5e1440)

--- src/sdp_convert/write_free_var_matrix.cxx
+++ src/sdp_convert/write_free_var_matrix.cxx
@@ -3,10 +3,10 @@
 #include <iostream>
 
 void write_free_var_matrix(std::ostream &output_stream,
-                           const size_t &dual_objectives_b_size,
                            const Dual_Constraint_Group &group)
 {
   size_t block_size(group.constraint_matrix.Height());
+  size_t width = group.constraint_matrix.Width();
 
   output_stream << "  \"B\":\n  [\n";
   for(size_t row = 0; row < block_size; ++row)
@@ -16,7 +16,7 @@ void write_free_var_matrix(std::ostream &output_stream,
           output_stream << ",\n";
         }
       output_stream << "    [\n";
-      for(size_t column = 0; column < dual_objectives_b_size; ++column)
+      for(size_t column = 0; column < width; ++column)
         {
           if(column != 0)
             {
