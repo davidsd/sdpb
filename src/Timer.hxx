386922099035f83e4b7379aea3cf713001b825f0
Vasiliy Dommes <vasdommes@gmail.com>
2023-11-09 15:14:22 -0500

    Fix Timer: show correct elapsed time if timer not stopped yet.
    Also require that Timer::stop() is called only once.
    + some refactoring

--- src/Timer.hxx
+++ src/Timer.hxx
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <chrono>
 #include <iostream>
 
@@ -7,25 +8,42 @@ struct Timer
 {
   std::chrono::time_point<std::chrono::high_resolution_clock> start_time,
     stop_time;
-  Timer() : start_time(std::chrono::high_resolution_clock::now()) {}
-  auto stop() { stop_time = std::chrono::high_resolution_clock::now(); }
-
-  int64_t elapsed_milliseconds() const
+  Timer() : start_time(now()) {}
+  auto stop()
   {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(stop_time
-                                                                 - start_time)
-      .count();
+    assert(is_running());
+    stop_time = now();
+    running = false;
   }
-  int64_t elapsed_seconds() const
+
+  [[nodiscard]] int64_t elapsed_milliseconds() const
   {
-    return std::chrono::duration_cast<std::chrono::seconds>(stop_time
-                                                            - start_time)
-      .count();
+    return elapsed<std::chrono::milliseconds>();
+  }
+  [[nodiscard]] int64_t elapsed_seconds() const
+  {
+    return elapsed<std::chrono::seconds>();
+  }
+
+private:
+  bool running = true;
+
+  [[nodiscard]] bool is_running() const { return running; }
+
+  static std::chrono::time_point<std::chrono::high_resolution_clock> now()
+  {
+    return std::chrono::high_resolution_clock::now();
+  }
+
+  template <class Duration> [[nodiscard]] int64_t elapsed() const
+  {
+    auto end_time = is_running() ? now() : stop_time;
+    return std::chrono::duration_cast<Duration>(end_time - start_time).count();
   }
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Timer &timer)
 {
-  os << (timer.elapsed_milliseconds()/1000.0);
+  os << (timer.elapsed_milliseconds() / 1000.0);
   return os;
 }
