9c480de5a27bee4b9d945e1bbdbf8e8a3a63d819
Vasiliy Dommes <vasdommes@gmail.com>
2023-11-17 23:43:40 -0500

    Add spectrum test case from mathematica/Tests.m/testSDPMatrix
    zeros - taken from https://gitlab.com/bootstrapcollaboration/spectrum-extraction/-/blob/master/test/spectrum_test_orig.m

--- test/src/integration_tests/cases/spectrum.test.cxx
+++ test/src/integration_tests/cases/spectrum.test.cxx
@@ -7,22 +7,27 @@ namespace fs = std::filesystem;
 TEST_CASE("spectrum")
 {
   INFO("Simple spectrum test");
-  Test_Util::Test_Case_Runner runner("spectrum");
 
-  fs::path data_dir = runner.data_dir;
-  fs::path output_dir = runner.output_dir;
+  for(std::string name : {"1d", "matrix"})
+    DYNAMIC_SECTION(name)
+    {
+      Test_Util::Test_Case_Runner runner("spectrum/" + name);
 
-  Test_Util::Test_Case_Runner::Named_Args_Map args{
-    {"--input", (data_dir / "pvm.xml").string()},
-    {"--solution", (data_dir / "solution").string()},
-    {"--output", (output_dir / "spectrum.json").string()},
-    {"--precision", "1024"},
-    {"--threshold", "1e-10"},
-    {"--format", "PVM"}};
+      fs::path data_dir = runner.data_dir;
+      fs::path output_dir = runner.output_dir;
 
-  runner.create_nested("run").mpi_run({"build/spectrum"}, args);
+      Test_Util::Test_Case_Runner::Named_Args_Map args{
+        {"--input", (data_dir / "pvm.xml").string()},
+        {"--solution", (data_dir / "solution").string()},
+        {"--output", (output_dir / "spectrum.json").string()},
+        {"--precision", "1024"},
+        {"--threshold", "1e-10"},
+        {"--format", "PVM"}};
 
-  auto out = output_dir / "spectrum.json";
-  auto out_orig = data_dir / "spectrum_orig.json";
-  Test_Util::REQUIRE_Equal::diff_spectrum(out, out_orig, 1024, 1024);
+      runner.mpi_run({"build/spectrum"}, args, 2);
+
+      auto out = output_dir / "spectrum.json";
+      auto out_orig = data_dir / "spectrum.orig.json";
+      Test_Util::REQUIRE_Equal::diff_spectrum(out, out_orig, 1024, 1024 / 2);
+    }
 }
