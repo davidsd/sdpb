54ed58fd1cbc356d30d06e92514fa351acddfe02
Vasiliy Dommes <vasdommes@gmail.com>
2024-01-08 01:47:44 -0500

    outer_limits.test.cxx: update test data, use pvm2functions
    TODO: obsolete test files moved to outer_limits/toy/ directory, review and use or delete them!
    TODO: test also sdp2functions
    TODO: add realistic tests, e.g. using end-to-end data

--- test/src/integration_tests/cases/outer_limits.test.cxx
+++ test/src/integration_tests/cases/outer_limits.test.cxx
@@ -7,24 +7,40 @@ namespace fs = std::filesystem;
 
 TEST_CASE("outer_limits")
 {
-  INFO("Simple outer_limits test");
-  Test_Util::Test_Case_Runner outer_runner("outer_limits");
-  fs::path data_dir = outer_runner.data_dir; //test/data/outer_limits
+  INFO("Simple pvm2functions + outer_limits test");
+  // TODO test also sdp2functions
 
   int num_procs = GENERATE(1, 2, 6);
   DYNAMIC_SECTION("num_procs=" << num_procs)
   {
-    Test_Util::Test_Case_Runner runner
-      = outer_runner.create_nested("mpirun-" + std::to_string(num_procs));
-
+    Test_Util::Test_Case_Runner runner("outer_limits/mpirun-"
+                                       + std::to_string(num_procs));
+    fs::path data_dir = runner.data_dir.parent_path(); //test/data/outer_limits
     fs::path output_dir = runner.output_dir;
 
+    fs::path functions_json = output_dir / "functions.json";
+    fs::path functions_orig_json = data_dir / "functions_orig.json";
+    int precision = 128;
+
+    {
+      INFO("run pvm2functions");
+      fs::path pvm_xml = Test_Config::test_data_dir / "pvm2sdp" / "pvm.xml";
+
+      // TODO allow running pvm2functions in parallel
+      runner.create_nested("pvm2functions")
+        .run({"build/pvm2functions", std::to_string(precision), pvm_xml,
+              functions_json});
+      Test_Util::REQUIRE_Equal::diff_functions_json(
+        functions_json, functions_orig_json, precision, precision / 2);
+    }
+
+    auto out_json = output_dir / "out.json";
     Test_Util::Test_Case_Runner::Named_Args_Map args{
-      {"--functions", (data_dir / "toy_functions.json").string()},
-      {"--out", (output_dir / "toy_functions_out.json").string()},
+      {"--functions", functions_json},
+      {"--out", out_json},
       {"--checkpointDir", (output_dir / "ck").string()},
-      {"--points", (data_dir / "toy_functions_points.json").string()},
-      {"--precision", "128"},
+      {"--points", (data_dir / "points.json").string()},
+      {"--precision", std::to_string(precision)},
       {"--dualityGapThreshold", "1e-10"},
       {"--primalErrorThreshold", "1e-10"},
       {"--dualErrorThreshold", "1e-10"},
@@ -34,10 +50,14 @@ TEST_CASE("outer_limits")
       {"--verbosity", "1"},
     };
 
-    runner.mpi_run({"build/outer_limits"}, args, num_procs);
+    {
+      INFO("run outer_limits");
+      runner.create_nested("outer_limits")
+        .mpi_run({"build/outer_limits"}, args, num_procs);
 
-    auto out = output_dir / "toy_functions_out.json";
-    auto out_orig = data_dir / "toy_functions_out_orig.json";
-    Test_Util::REQUIRE_Equal::diff_outer_limits(out, out_orig, 128, 128);
+      auto out_orig_json = data_dir / "out_orig.json";
+      Test_Util::REQUIRE_Equal::diff_outer_limits(out_json, out_orig_json,
+                                                  precision, precision / 2);
+    }
   }
 }
