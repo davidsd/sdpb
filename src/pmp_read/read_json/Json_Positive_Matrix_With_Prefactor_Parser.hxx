069c3f875f5cf710bc00253a928073e11fd81fed
Vasiliy Dommes <vasdommes@gmail.com>
2024-02-09 18:32:18 -0500

    Add optional samplePoints field to pmp.json (+test)
    This is a part of #179 Universal input format for Polynomial Matrix Program
    
    TODO: write it from in SDPB.m
    TODO: update docs

--- src/pmp_read/read_json/Json_Positive_Matrix_With_Prefactor_Parser.hxx
+++ src/pmp_read/read_json/Json_Positive_Matrix_With_Prefactor_Parser.hxx
@@ -22,6 +22,7 @@ class Json_Positive_Matrix_With_Prefactor_Parser final
 private:
   std::optional<Matrix_Of_Polynomial_Vectors> polynomials;
   std::optional<Damped_Rational> prefactor;
+  std::optional<std::vector<El::BigFloat>> sample_points;
 
 public:
   Json_Positive_Matrix_With_Prefactor_Parser(
@@ -33,14 +34,20 @@ public:
                            [this](Matrix_Of_Polynomial_Vectors &&pvm) {
                              polynomials = std::make_optional(std::move(pvm));
                            }),
-        prefactor_parser(skip, [this](Damped_Rational &&prefactor) {
-          this->prefactor = std::move(prefactor);
-        })
+        prefactor_parser(skip,
+                         [this](Damped_Rational &&prefactor) {
+                           this->prefactor = std::move(prefactor);
+                         }),
+        sample_points_parser(
+          skip, [this](std::vector<El::BigFloat> &&sample_points) {
+            this->sample_points = std::move(sample_points);
+          })
   {}
 
 private:
   Json_Matrix_Of_Polynomial_Vectors_Parser polynomials_parser;
   Json_Damped_Rational_Parser prefactor_parser;
+  Json_Float_Vector_Parser<El::BigFloat> sample_points_parser;
 
 protected:
   Abstract_Json_Reader_Handler &element_parser(const std::string &key) override
@@ -49,6 +56,8 @@ protected:
       return polynomials_parser;
     if(key == "DampedRational")
       return prefactor_parser;
+    if(key == "samplePoints")
+      return sample_points_parser;
     RUNTIME_ERROR("unknown key=", key);
   }
 
@@ -58,18 +67,20 @@ public:
     ASSERT(polynomials.has_value(), "polynomials not found");
     const auto matrix = to_matrix(std::move(polynomials).value());
     // TODO add move ctor for Polynomial_Vector_Matrix?
-    return Polynomial_Vector_Matrix(matrix, prefactor, std::nullopt,
+    return Polynomial_Vector_Matrix(matrix, prefactor, sample_points,
                                     std::nullopt, std::nullopt);
   }
   void clear_result() override
   {
     polynomials.reset();
     prefactor.reset();
+    sample_points.reset();
   }
   void reset_element_parsers(const bool skip) override
   {
     prefactor_parser.reset(skip);
     polynomials_parser.reset(skip);
+    sample_points_parser.reset(skip);
   }
 };
 
