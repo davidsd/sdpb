3ef4bb01abd0669dec3821f653e32a0303d9c497
Vasiliy Dommes <vasdommes@gmail.com>
2024-04-09 20:38:21 -0400

    sample_scalings(): regularize Damped_Rational if point is close to a pole
    TODO: which threshold to choose? Currently, we use 1e-16 (without any justification).

--- src/pmp/convert/sample_scalings.cxx
+++ src/pmp/convert/sample_scalings.cxx
@@ -6,13 +6,20 @@
 
 #include <vector>
 
-// Rescaled Laguerre
 std::vector<Boost_Float>
 sample_scalings(const std::vector<Boost_Float> &points,
                 const Damped_Rational &damped_rational)
 {
   std::vector<Boost_Float> result;
   result.reserve(points.size());
+
+  // Evaluate damped_rational at given points.
+  // If a point x is too close to a pole p,
+  // we replace (x - p) with min_pole_distance.
+
+  // TODO choose better threshold and/or make it configurable
+  const Boost_Float min_pole_distance = 1e-16;
+
   for(auto &point : points)
     {
       Boost_Float numerator(damped_rational.constant
@@ -20,7 +27,17 @@ sample_scalings(const std::vector<Boost_Float> &points,
       Boost_Float denominator(1);
       for(auto &pole : damped_rational.poles)
         {
-          denominator *= (point - pole);
+          Boost_Float delta = point - pole;
+
+          // Regularize value at small denominator
+          if(abs(delta) < min_pole_distance)
+            {
+              delta = min_pole_distance;
+              if(sign(delta) < 0)
+                delta = -delta;
+            }
+
+          denominator *= delta;
         }
       result.emplace_back(numerator / denominator);
     }
