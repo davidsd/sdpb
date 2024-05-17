f3670c2659c02169ba63bff9493016f6454b1702
Vasiliy Dommes <vasdommes@gmail.com>
2024-05-17 16:19:32 -0400

    Add LIBPATH to RPATH for libraries found via pkg-config
    The same as we do when searching in default paths, by calling conf.check_cxx(libpath=libpath, rpath=libpath)
    
    Prevents runtime error e.g. on Caltech HPC:
    ./build/sdpb: error while loading shared libraries: libgmpxx.so.4: cannot open shared object file: No such file or directory

--- waf-tools/check_config.py
+++ waf-tools/check_config.py
@@ -10,11 +10,20 @@ def check_pkg_config(conf, package, uselib_store, fragment, use=None, msg_prefix
                           uselib_store=uselib_store,
                           **kwargs):
         return False
+
     # check_cfg() does not support 'fragment' argument,
     # so we have to check compilation separately via check_cxx()
+
+    # We also set rpath=libpath, same as we do in check_config() below
+    # You can find LIBPATH values (set by conf.check_cfg)
+    # in build/c4che/_cache.py, e.g.
+    # LIBPATH_cblas = ['/usr/lib/x86_64-linux-gnu/openblas-pthread/']
+    libpath = getattr(conf.env, 'LIBPATH_' + uselib_store, None)
     return conf.check_cxx(msg=msg_prefix + 'Checking ' + package + ' compilation',
                           fragment=fragment,
+                          rpath=libpath,
                           use=(use or []) + [uselib_store],
+                          uselib_store=uselib_store,
                           **kwargs)
 
 
