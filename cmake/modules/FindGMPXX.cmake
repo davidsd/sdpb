4f652ef28e229d4fb3c4feed96f38a4fb07390ac
Bharathkumar Radhakrishnan <r.bharathkumar@outlook.com>
2024-06-17 11:37:01 +0200

    Cleanup of paths to match with upstream changes

--- cmake/modules/FindGMPXX.cmake
+++ cmake/modules/FindGMPXX.cmake
@@ -7,7 +7,6 @@ find_library(GMPXX_LIBRARIES gmpxx
                     PATHS $ENV{GMPXXDIR} $ENV{GMPXX_HOME} $ENV{GMPXX_LIB} $ENV{GMPDIR} $ENV{GMP_HOME} $ENV{GMP_LIB} ${LIB_INSTALL_DIR}
                     )
 
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GMPXX DEFAULT_MSG
                                     GMPXX_INCLUDE_DIR GMPXX_LIBRARIES)
