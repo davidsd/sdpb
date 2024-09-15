d884344c83fdc08f61f2ce01bab765e621081d9c
Vasiliy Dommes <vasdommes@gmail.com>
2024-09-15 16:26:02 -0400

    spectrum: for --verbosity=trace, write profiling to JSON each time when a block finished
    It could be helpful to look at profiling for unfinished runs,
    if some blocks take much longer than expected.

--- src/spectrum/write_profiling.cxx
+++ src/spectrum/write_profiling.cxx
@@ -3,12 +3,19 @@
 
 namespace fs = std::filesystem;
 
-void write_profiling(const fs::path &spectrum_output_path, Timers &timers)
+namespace
 {
-  Scoped_Timer write_profiling_timer(timers, "write_profiling");
+  fs::path get_profiling_dir(const fs::path &spectrum_output_path)
+  {
+    fs::path profiling_dir = spectrum_output_path;
+    profiling_dir += ".profiling";
+    return profiling_dir;
+  }
+}
 
-  fs::path profiling_dir = spectrum_output_path;
-  profiling_dir += ".profiling";
+void create_profiling_dir(const fs::path &spectrum_output_path)
+{
+  const auto profiling_dir = get_profiling_dir(spectrum_output_path);
   if(El::mpi::Rank() == 0)
     {
       std::error_code ec;
@@ -17,10 +24,17 @@ void write_profiling(const fs::path &spectrum_output_path, Timers &timers)
         PRINT_WARNING("Failed to create directory ", profiling_dir, ": ",
                       ec.message());
     }
+  // Wait until directory is created
+  El::mpi::Barrier();
+}
+
+void write_profiling(const fs::path &spectrum_output_path, Timers &timers)
+{
+  Scoped_Timer write_profiling_timer(timers, "write_profiling");
+
+  const auto profiling_dir = get_profiling_dir(spectrum_output_path);
   try
     {
-      // Wait until directory is created
-      El::mpi::Barrier();
       ASSERT(fs::exists(profiling_dir) && fs::is_directory(profiling_dir),
              DEBUG_STRING(profiling_dir));
       timers.write_profile(profiling_dir
