3363c18fc9add4a24117582c9707e4ac1336976c
Vasiliy Dommes <vasdommes@gmail.com>
2024-11-01 22:49:18 -0400

    fix typos in comments in restore_and_reduce.cxx

--- src/sdp_solve/SDP_Solver/run/bigint_syrk/BigInt_Shared_Memory_Syrk_Context/restore_and_reduce.cxx
+++ src/sdp_solve/SDP_Solver/run/bigint_syrk/BigInt_Shared_Memory_Syrk_Context/restore_and_reduce.cxx
@@ -3,7 +3,7 @@
 #include "restore_bigint_from_residues.hxx"
 #include "sdpb_util/block_mapping/MPI_Comm_Wrapper.hxx"
 
-// Restore output maxtrix from residues (outpet_residues_window) and synchronize it for all nodes.
+// Restore output matrix from residues (output_residues_window) and synchronize it for all nodes.
 //
 // Initially, each node n has its own matrix Q_n, stored as residues in output_residues_window.
 // The function restores Q_n from residues, sums Q_n over all nodes
@@ -11,11 +11,11 @@
 //
 // Algorithm:
 // 1. The rank that owns the element Q[i,j] sets Q[i,j] = Q_n[i,j] restored from residues on its node.
-// 2. Accumulate Q_n[i,j] from other nodes to Q[i,j] according to the followin scheme:
+// 2. Accumulate Q_n[i,j] from other nodes to Q[i,j] according to the following scheme:
 //   for offset = 1..num_nodes-1:
 //   - Each node (n) restores all Q_n[i,j] for [i,j] owned by node (n+offset) from residues,
 //     and sends them to node (n+offset). Implemented via MPI_Sendrecv.
-//   - Each node updates its own Q[i,j] with data recevied from node (n-offset).
+//   - Each node updates its own Q[i,j] with data received from node (n-offset).
 //
 // All ranks on a node have access to each Q_n[i,j], so how do we decide
 //   which rank should send it?
@@ -176,7 +176,7 @@ void BigInt_Shared_Memory_Syrk_Context::restore_and_reduce(
                        DEBUG_STRING(width), DEBUG_STRING(global_ranks.at(to)));
         }
 
-        // Receive buffer will recieve elements for a current rank
+        // Receive buffer will receive elements for a current rank
         recv_buf.resize(num_output_elements.at(reduce_rank) * serialized_size);
 
         {
