efaaed450b24c258ae4cde3cb2d51ab3e92a5089
Vasiliy Dommes <vasdommes@gmail.com>
2024-12-26 20:09:44 -0500

    Parse Mathematica numbers without precision mark, e.g. 1.2345*^-67
    TODO: add pmp2sdp tests.

--- src/pmp_read/read_mathematica/parse_SDP/parse_number.cxx
+++ src/pmp_read/read_mathematica/parse_SDP/parse_number.cxx
@@ -1,32 +1,52 @@
 #include "is_valid_char.hxx"
+#include "sdpb_util/assert.hxx"
 
-#include <algorithm>
-#include <vector>
 #include <string>
 
+const char *next_valid_char(const char *c, const char *end)
+{
+  do
+    {
+      ++c;
+      if(is_valid_char(*c))
+        break;
+  } while(c != end);
+  return c;
+}
+
+// "-1.234`8.45 *^ -10" -> "-1.234e-10"
 std::string parse_number(const char *begin, const char *end)
 {
   std::string cleaned_string;
   cleaned_string.reserve(end - begin);
-  auto c(begin);
-  for(; c != end && *c != '`'; ++c)
+
+  auto c = begin;
+
+  // Go until precision mark "`" or exponent "*^"
+  for(; c != end; c = next_valid_char(c, end))
     {
-      if(is_valid_char(*c))
-        {
-          cleaned_string.push_back(*c);
-        }
+      if(*c == '`' || *c == '*')
+        break;
+      cleaned_string.push_back(*c);
     }
-  auto carat(std::find(c, end, '^'));
-  if(carat != end)
+
+  // Skip precision mark:
+  if(*c == '`')
+    c = std::find(c, end, '*');
+  if(c == end)
+    return cleaned_string;
+
+  // Replace "*^" with "e"
+  c = next_valid_char(c, end);
+  ASSERT_EQUAL(*c, '^');
+  c = next_valid_char(c, end);
+  cleaned_string.push_back('e');
+
+  // Add exponent part
+  for(; c != end; c = next_valid_char(c, end))
     {
-      cleaned_string.push_back('e');
-      for(auto c(std::next(carat)); c != end; ++c)
-        {
-          if(is_valid_char(*c))
-            {
-              cleaned_string.push_back(*c);
-            }
-        }
+      cleaned_string.push_back(*c);
     }
+
   return cleaned_string;
 }
