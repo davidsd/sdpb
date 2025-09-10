92eaca835c86d1c772e3f80731c5d59e36946a49
Vasiliy Dommes <vasdommes@gmail.com>
2025-09-10 18:38:10 -0400

    Extract max_block_cholesky_condition_number() to sdbp_util/cholesky_condition_number.hxx

--- src/sdp_solve/SDP_Solver/run/step/update_cond_numbers.hxx
+++ src/sdp_solve/SDP_Solver/run/step/update_cond_numbers.hxx
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "sdp_solve/Block_Diagonal_Matrix.hxx"
+#include "sdp_solve/Block_Matrix/Block_Diagonal_Matrix.hxx"
 #include "sdpb_util/assert.hxx"
 #include "sdp_solve/Block_Info.hxx"
 #include "sdpb_util/cholesky_condition_number.hxx"
@@ -12,16 +12,14 @@
 // Q_cond_number - condition number Q_cond_number for Cholesky decomposed Q
 // max_block_cond_number - max condition number among all blocks of schur_complement_cholesky, X_cholesky,Y_cholesky
 // max_block_cond_number_name - name of the corresponding block
-inline void
-update_cond_numbers(const El::DistMatrix<El::BigFloat> &Q,
-                    const Block_Info &block_info,
-                    const Block_Diagonal_Matrix &schur_complement_cholesky,
-                    const Paired_Block_Diagonal_Matrix &X_cholesky,
-                    const Paired_Block_Diagonal_Matrix &Y_cholesky, Timers &timers,
-                    // Output variables:
-                    El::BigFloat &Q_cond_number,
-                    El::BigFloat &max_block_cond_number,
-                    std::string &max_block_cond_number_name)
+inline void update_cond_numbers(
+  const El::DistMatrix<El::BigFloat> &Q, const Block_Info &block_info,
+  const Block_Diagonal_Matrix &schur_complement_cholesky,
+  const Paired_Block_Diagonal_Matrix &X_cholesky,
+  const Paired_Block_Diagonal_Matrix &Y_cholesky, Timers &timers,
+  // Output variables:
+  El::BigFloat &Q_cond_number, El::BigFloat &max_block_cond_number,
+  std::string &max_block_cond_number_name)
 {
   Scoped_Timer cond_number_timer(timers, "condition_numbers");
 
@@ -33,77 +31,48 @@ update_cond_numbers(const El::DistMatrix<El::BigFloat> &Q,
   // schur_complement_cholesky
 
   const size_t num_blocks = block_info.dimensions.size();
-  std::vector<El::BigFloat> schur_complement_cholesky_cond(num_blocks);
-  for(size_t local_block_index = 0;
-      local_block_index < schur_complement_cholesky.blocks.size();
-      ++local_block_index)
-    {
-      const auto &cholesky_block
-        = schur_complement_cholesky.blocks.at(local_block_index);
-      const auto global_block_index
-        = block_info.block_indices.at(local_block_index);
-
-      schur_complement_cholesky_cond.at(global_block_index)
-        = cholesky_condition_number(cholesky_block);
-    }
+  auto get_schur_block_index = [&](const size_t local_block_index) {
+    return block_info.block_indices.at(local_block_index);
+  };
+  auto [schur_complement_index, schur_complement_cholesky_cond]
+    = max_block_cholesky_condition_number(schur_complement_cholesky.blocks,
+                                          num_blocks, get_schur_block_index);
 
   // X_cholesky, Y_cholesky
 
   const auto num_X_blocks = 2 * num_blocks;
-  std::vector<El::BigFloat> X_cholesky_cond(num_X_blocks);
-  std::vector<El::BigFloat> Y_cholesky_cond(num_X_blocks);
-  ASSERT_EQUAL(X_cholesky.blocks.size(), 2 * block_info.block_indices.size());
-  ASSERT_EQUAL(Y_cholesky.blocks.size(), 2 * block_info.block_indices.size());
-  for(size_t i = 0; i < block_info.block_indices.size(); ++i)
-    {
-      const auto block_index = block_info.block_indices.at(i);
-      for(const size_t parity : {0, 1})
-        {
-          const auto X_index = 2 * block_index + parity;
-          X_cholesky_cond.at(X_index)
-            = cholesky_condition_number(X_cholesky.get_block(i, parity));
-          Y_cholesky_cond.at(X_index)
-            = cholesky_condition_number(Y_cholesky.get_block(i, parity));
-        }
-    }
+  std::function get_X_index = [&block_info](size_t local_X_index) {
+    const size_t parity = local_X_index % 2;
+    return 2 * block_info.block_indices.at(local_X_index / 2) + parity;
+  };
+  auto [X_index, X_cholesky_cond] = max_block_cholesky_condition_number(
+    X_cholesky.blocks, num_X_blocks, get_X_index);
+  auto [Y_index, Y_cholesky_cond] = max_block_cholesky_condition_number(
+    Y_cholesky.blocks, num_X_blocks, get_X_index);
 
   // Synchronize all at rank 0
 
-  El::mpi::Reduce(schur_complement_cholesky_cond.data(), num_blocks,
-                  El::mpi::MAX, 0, El::mpi::COMM_WORLD);
-  El::mpi::Reduce(X_cholesky_cond.data(), num_X_blocks, El::mpi::MAX, 0,
-                  El::mpi::COMM_WORLD);
-  El::mpi::Reduce(Y_cholesky_cond.data(), num_X_blocks, El::mpi::MAX, 0,
-                  El::mpi::COMM_WORLD);
-
   if(El::mpi::Rank() == 0)
     {
-      for(size_t block_index = 0; block_index < num_blocks; ++block_index)
+      max_block_cond_number = schur_complement_cholesky_cond;
+      max_block_cond_number_name = "schur_complement_cholesky.block_"
+                                   + std::to_string(schur_complement_index);
+
+      if(max_block_cond_number < X_cholesky_cond)
         {
-          if(max_block_cond_number
-             < schur_complement_cholesky_cond.at(block_index))
-            {
-              max_block_cond_number
-                = schur_complement_cholesky_cond.at(block_index);
-              max_block_cond_number_name = "schur_complement_cholesky.block_"
-                                           + std::to_string(block_index);
-            }
-          for(const size_t parity : {0, 1})
-            {
-              const auto X_index = 2 * block_index + parity;
-              if(max_block_cond_number < X_cholesky_cond.at(X_index))
-                {
-                  max_block_cond_number = X_cholesky_cond.at(X_index);
-                  max_block_cond_number_name = El::BuildString(
-                    "X_cholesky.block_", block_index, "_", parity);
-                }
-              if(max_block_cond_number < Y_cholesky_cond.at(X_index))
-                {
-                  max_block_cond_number = Y_cholesky_cond.at(X_index);
-                  max_block_cond_number_name = El::BuildString(
-                    "Y_cholesky.block_", block_index, "_", parity);
-                }
-            }
+          max_block_cond_number = X_cholesky_cond;
+          const auto block_index = X_index / 2;
+          const auto parity = X_index % 2;
+          max_block_cond_number_name
+            = El::BuildString("X_cholesky.block_", block_index, "_", parity);
+        }
+      if(max_block_cond_number < Y_cholesky_cond)
+        {
+          max_block_cond_number = Y_cholesky_cond;
+          const auto block_index = Y_index / 2;
+          const auto parity = Y_index % 2;
+          max_block_cond_number_name
+            = El::BuildString("X_cholesky.block_", block_index, "_", parity);
         }
     }
 }
--- src/sdpb_util/cholesky_condition_number.hxx
+++ src/sdpb_util/cholesky_condition_number.hxx
@@ -2,6 +2,7 @@
 
 #include "assert.hxx"
 #include <El.hpp>
+#include <optional>
 
 // Condition number for Cholesky matrix can be estimated as a squared ratio of maximal and minimal elements on its diagonal,
 // see https://scicomp.stackexchange.com/questions/32762/cholmod-condition-number-estimate
@@ -34,4 +35,47 @@ T cholesky_condition_number(const El::AbstractDistMatrix<T> &matrix)
 
   auto ratio = max_diagonal_element / min_diagonal_element;
   return ratio * ratio;
+}
+
+// Returns max condition number among all blocks a distributed block-diagonal matrix
+// and returns them on rank = 0.
+// Returns {-1, -1} on other ranks
+template <class TDistMatrix>
+std::pair<int, El::BigFloat> max_block_cholesky_condition_number(
+  const std::vector<TDistMatrix> &local_blocks, const size_t num_blocks,
+  const std::function<size_t(size_t)> get_block_index)
+{
+  El::BigFloat zero(0);
+
+  El::DistMatrix<El::BigFloat, El::STAR, El::STAR> tttt = local_blocks.at(0);
+  El::DistMatrix<El::BigFloat, El::CIRC, El::CIRC> wwww = local_blocks.at(0);
+
+  std::vector<El::BigFloat> cond_numbers(num_blocks, zero);
+  for(size_t i = 0; i < local_blocks.size(); ++i)
+    {
+      const auto block_index = get_block_index(i);
+      ASSERT(block_index < num_blocks, DEBUG_STRING(block_index),
+             DEBUG_STRING(num_blocks));
+      cond_numbers.at(block_index)
+        = cholesky_condition_number(local_blocks.at(i));
+    }
+
+  // Synchronize all at rank 0
+  El::mpi::Reduce(cond_numbers.data(), num_blocks, El::mpi::MAX, 0,
+                  El::mpi::COMM_WORLD);
+
+  El::BigFloat max_cond_number = -1;
+  int max_block_index = -1;
+  if(El::mpi::Rank() == 0)
+    {
+      for(size_t i = 0; i < num_blocks; ++i)
+        {
+          if(cond_numbers.at(i) > max_cond_number)
+            {
+              max_cond_number = cond_numbers.at(i);
+              max_block_index = i;
+            }
+        }
+    }
+  return {max_block_index, max_cond_number};
 }
