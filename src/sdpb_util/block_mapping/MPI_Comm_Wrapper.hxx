31953dc41fdeccc5f2a679f27fa171aea3e2605d
Vasiliy Dommes <vasdommes@gmail.com>
2025-09-13 08:32:40 -0400

    Revert "Remove unnecessary swap() definitions, use move operations instead."
    This reverts commit ec6c5b597b7bb01ef476868ddf0828c24c4b663f.
    
    Default std::swap for BLock_Info was sometimes causing crash with:
    free(): corrupted unsorted chunks

--- src/sdpb_util/block_mapping/MPI_Comm_Wrapper.hxx
+++ src/sdpb_util/block_mapping/MPI_Comm_Wrapper.hxx
@@ -6,12 +6,8 @@ struct MPI_Comm_Wrapper
 {
   El::mpi::Comm value;
   MPI_Comm_Wrapper() = default;
-  // Allow move
-  MPI_Comm_Wrapper(MPI_Comm_Wrapper &&other) noexcept = default;
-  MPI_Comm_Wrapper &operator=(MPI_Comm_Wrapper &&other) noexcept = default;
-  // Prohibit copy
   MPI_Comm_Wrapper(const MPI_Comm_Wrapper &) = delete;
-  MPI_Comm_Wrapper &operator=(const MPI_Comm_Wrapper &) = delete;
+  void operator=(const MPI_Comm_Wrapper &) = delete;
   ~MPI_Comm_Wrapper()
   {
     if(value != El::mpi::COMM_WORLD)
@@ -20,3 +16,11 @@ struct MPI_Comm_Wrapper
       }
   }
 };
+
+namespace std
+{
+  inline void swap(MPI_Comm_Wrapper &a, MPI_Comm_Wrapper &b) noexcept
+  {
+    swap(a.value, b.value);
+  }
+}
--- src/sdpb_util/block_mapping/MPI_Group_Wrapper.hxx
+++ src/sdpb_util/block_mapping/MPI_Group_Wrapper.hxx
@@ -5,15 +5,9 @@
 struct MPI_Group_Wrapper
 {
   El::mpi::Group value;
-
   MPI_Group_Wrapper() = default;
-  // Allow move
-  MPI_Group_Wrapper(MPI_Group_Wrapper &&other) noexcept = default;
-  MPI_Group_Wrapper &operator=(MPI_Group_Wrapper &&other) noexcept = default;
-  // Prohibit copy
   MPI_Group_Wrapper(const MPI_Group_Wrapper &) = delete;
-  MPI_Group_Wrapper &operator=(const MPI_Group_Wrapper &) = delete;
-
+  void operator=(const MPI_Group_Wrapper &) = delete;
   ~MPI_Group_Wrapper()
   {
     if(value != El::mpi::GROUP_NULL)
@@ -22,3 +16,11 @@ struct MPI_Group_Wrapper
       }
   }
 };
+
+namespace std
+{
+  inline void swap(MPI_Group_Wrapper &a, MPI_Group_Wrapper &b) noexcept
+  {
+    swap(a.value, b.value);
+  }
+}
