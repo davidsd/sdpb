67f9050bf3e27fc94024fff8e7308c2cd52ec08c
Vasiliy Dommes <vasdommes@gmail.com>
2025-09-23 17:41:59 -0400

    sdpa solver: call Trmm and Trsm once per SDP block

--- src/sdpa_solve/SDP_Solver/run/step/compute_S.cxx
+++ src/sdpa_solve/SDP_Solver/run/step/compute_S.cxx
@@ -25,7 +25,10 @@ namespace Sdpb::Sdpa
                El::Matrix<int32_t> &block_timings_ms,
                const Verbosity verbosity)
   {
-    // Set matrix dimensions for P
+    Scoped_Timer timer(timers, "initialize_P");
+    const size_t primal_dimension = block_info.primal_dimension;
+
+    // Set matrix dimensions for output Block_Matrix P
     // Each block is a (dim*dim) x primal_dimension matrix
     std::vector<size_t> P_block_heights;
     P_block_heights.reserve(block_info.num_blocks());
@@ -33,7 +36,7 @@ namespace Sdpb::Sdpa
       {
         P_block_heights.emplace_back(dim * dim);
       }
-    const size_t P_width = block_info.primal_dimension;
+    const size_t P_width = primal_dimension;
     Block_Matrix result(P_block_heights, P_width, block_info.block_indices,
                         grid);
 
@@ -41,36 +44,83 @@ namespace Sdpb::Sdpa
       {
         const auto global_block_index = block_info.block_indices.at(block);
         auto block_index_string = std::to_string(global_block_index);
+        const size_t block_dim
+          = block_info.block_dimensions.at(global_block_index);
 
         Scoped_Timer solve_timer(timers, "solve_" + block_index_string);
+
+        // Compute G_p = L_X_Inv F_p L_Y
+        // start with F_p
+
+        // Take blocks with current block index from F_1,F_2,..F_m
+        // and put them on top of each other.
+        El::DistMatrix<El::BigFloat> FY_block_vertical(
+          block_dim * primal_dimension, block_dim, grid);
+        for(size_t p = 0; p < primal_dimension; ++p)
+          {
+            const auto &source = sdp.sdp_blocks_F.at(p).blocks.at(block);
+            const El::Range<int> I(p * block_dim, (p + 1) * block_dim);
+            const El::Range<int> J(0, block_dim);
+            auto dest = El::View(FY_block_vertical, I, J);
+            dest = source;
+            // sanity check
+            ASSERT(dest.Viewing());
+          }
+
+        // compute F_p L_Y
+        {
+          Scoped_Timer trmm_timer(timers, "trmm");
+          El::Trmm(El::LeftOrRightNS::RIGHT, El::UpperOrLowerNS::LOWER,
+                   El::OrientationNS::NORMAL, El::UnitOrNonUnitNS::NON_UNIT,
+                   El::BigFloat(1), Y_cholesky.blocks.at(block),
+                   FY_block_vertical);
+        }
+
+        // compute L_X_inv F_p L_Y
+
+        // Arrange blocks horizontally instead of vertically.
+        // NB: ve do not transpose each block itself!
+        Scoped_Timer transpose_G_timer(timers, "transpose_G");
+        El::DistMatrix<El::BigFloat> G_block_horizontal(
+          block_dim, block_dim * primal_dimension, grid);
+        for(size_t p = 0; p < primal_dimension; ++p)
+          {
+            const El::Range<int> I(p * block_dim, (p + 1) * block_dim);
+            const El::Range<int> J(0, block_dim);
+
+            const auto source = FY_block_vertical(I, J);
+            // NB: I <-> J
+            auto dest = El::View(G_block_horizontal, J, I);
+            dest = source;
+            //sanity check
+            ASSERT(dest.Viewing());
+          }
+        transpose_G_timer.stop();
+
+        {
+          Scoped_Timer trsm_timer(timers, "trsm");
+
+          El::Trsm(El::LeftOrRightNS::LEFT, El::UpperOrLowerNS::LOWER,
+                   El::OrientationNS::NORMAL, El::UnitOrNonUnitNS::NON_UNIT,
+                   El::BigFloat(1), X_cholesky.blocks.at(block),
+                   G_block_horizontal);
+        }
+
+        Scoped_Timer reshape_timer(timers, "reshape");
         for(size_t p = 0; p < sdp.primal_dimension(); ++p)
           {
-            // Compute G_p = L_X_Inv F_p L_Y
-            // start with F_p
-            El::DistMatrix<El::BigFloat> G_p_block
-              = sdp.sdp_blocks_F.at(p).blocks.at(block);
-            // compute F_p L_Y
-            El::Trmm(El::LeftOrRightNS::RIGHT, El::UpperOrLowerNS::LOWER,
-                     El::OrientationNS::NORMAL, El::UnitOrNonUnitNS::NON_UNIT,
-                     El::BigFloat(1), Y_cholesky.blocks.at(block), G_p_block);
-            // compute L_X_inv F_p L_Y
-            El::Trsm(El::LeftOrRightNS::LEFT, El::UpperOrLowerNS::LOWER,
-                     El::OrientationNS::NORMAL, El::UnitOrNonUnitNS::NON_UNIT,
-                     El::BigFloat(1), X_cholesky.blocks.at(block), G_p_block);
-            // TODO: can we utilize El::View somehow to have a single El::Trmm (and Trsm) call for all F_i?
-            // This would require putting all blocks into a single DistMatrix:
-            // Each block of F is smth like a Block_Matrix, with F_i stacked on top of each other
-            // Internally, it is stored as a single DistMatrix.
-            // There should be a getter for F_i providing a view to this DistMatrix.
+            const El::Range<int> in_I(0, block_dim);
+            const El::Range<int> in_J(p * block_dim, (p + 1) * block_dim);
+            const auto G_p_block = El::View(G_block_horizontal, in_I, in_J);
 
             auto &P_block = result.blocks.at(block);
             // Write all elements of G_p into the p-th column of the resulting P matrix
             const int vec_height = P_block.Height();
             constexpr int vec_width = 1;
-            const auto I = El::Range<int>(0, vec_height);
-            const auto J = El::Range<int>(p, p + vec_width);
+            const auto out_I = El::Range<int>(0, vec_height);
+            const auto out_J = El::Range<int>(p, p + vec_width);
 
-            auto vec_G_p_view = El::View(P_block, I, J);
+            auto vec_G_p_view = El::View(P_block, out_I, out_J);
             // TODO how does the resulting DistMatrix grid change after reshape?
             // Will the elements be evenly distributed among ranks?
             El::Reshape(vec_height, vec_width, G_p_block, vec_G_p_view);
@@ -78,6 +128,7 @@ namespace Sdpb::Sdpa
         block_timings_ms(global_block_index, 0)
           += solve_timer.elapsed_milliseconds();
       }
+
     if(verbosity >= Verbosity::trace)
       {
         print_allocation_message_per_node(env, "P",
