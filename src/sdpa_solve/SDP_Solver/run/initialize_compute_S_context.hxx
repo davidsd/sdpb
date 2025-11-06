3220963df25b618fcfb6f1d8b5b2a2c95e5b3ba5
Vasiliy Dommes <vasdommes@gmail.com>
2025-11-05 22:42:04 -0500

    initialize_compute_S_context.hxx: cleanup unused code

--- src/sdpa_solve/SDP_Solver/run/initialize_compute_S_context.hxx
+++ src/sdpa_solve/SDP_Solver/run/initialize_compute_S_context.hxx
@@ -1,14 +1,9 @@
 #pragma once
 
-#include "sdp_solve/Block_Matrix/Block_Matrix.hxx"
-#include "sdpa_solve/Block_Info.hxx"
-#include "sdpa_solve/SDP.hxx"
-#include "sdpa_solve/SDP_Solver/run/step/Initialize_P_Context.hxx"
 #include "sdp_solve/SDP_Solver/run/get_syrk_Q_config.hxx"
 #include "sdp_solve/SDP_Solver/run/bigint_syrk/BigInt_Shared_Memory_Syrk_Context.hxx"
-#include "sdpb_util/assert.hxx"
-#include "sdpb_util/bigint_shared_memory/Vector_Matrix_Residues_Window.hxx"
-#include "sdp_solve/SDP_Solver/run/bigint_syrk/Bigint_Syrk_Config.hxx"
+#include "sdpa_solve/Block_Info.hxx"
+#include "sdpa_solve/SDP_Solver/run/step/Initialize_P_Context.hxx"
 #include "step/Compute_S_Config.hxx"
 #include "step/Initialize_P_Config.hxx"
 
@@ -32,10 +27,6 @@ namespace Sdpb::Sdpa
                                const Verbosity verbosity)
   {
     const auto group_index = block_info.node_group_index();
-    const auto precision = El::gmp::Precision();
-
-    const auto block_width = block_info.primal_dimension;
-
     std::vector<int> group_comm_sizes;
     std::vector<int> blocks_height_per_group;
     for(const auto &group :
